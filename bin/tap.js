bbdfdac23b50a17665a805f7eec04e7ca051468d
isaacs <i@izs.me>
2011-03-18 15:42:02 -0700

    test dirs, and exit non-zero when tests fail

--- bin/tap.js
+++ bin/tap.js
@@ -1,48 +1,11 @@
 #!/usr/bin/env node
 
-var fs = require("fs")
-  , argv = process.argv.slice(2)
-  , child_process = require("child_process")
+var argv = process.argv.slice(2)
   , path = require("path")
-  , TapStream = require("../lib/tap-stream")
-  , ts = new TapStream()
   , dir = path.resolve(argv[0])
+  , testDir = require("../lib/test-dir")
 
-ts.pipe(process.stdout)
-
-fs.readdir(dir, function (er, files) {
+testDir(dir, !process.env.nodiag, function (er, total, ok) {
   if (er) throw er
-  var count = files.length
-
-  files.forEach(function (f) {
-    f = path.resolve(dir, f)
-    fs.stat(f, function (er, st) {
-      if (er) throw er
-      if (st.isDirectory()) {
-        console.error("nested dirs not supported yet.")
-        return
-      }
-      var cmd = f
-        , args = []
-      if (path.basename(f) === ".js") {
-        cmd = "node"
-        args = [f]
-      }
-      var cp = child_process.spawn(cmd, args, { cwd: dir })
-        , out = ""
-        , err = ""
-      cp.stdout.on("data", function (c) { out += c })
-      cp.stderr.on("data", function (c) { err += c })
-      cp.on("exit", function (code) {
-        ts.write(f)
-        // TODO: If the output is TAP, then be smart with it.
-        // If the output is a yamlish object, then parse that, too.
-        ts.write({ name: f.substr(dir.length + 1)
-                 , ok: !code
-                 , output: out
-                 , stderr: err })
-        if (-- count === 0) ts.end()
-      })
-    })
-  })
-})
+  process.exit(total - ok)
+}).pipe(process.stdout)
---
+++ test/common.js
@@ -0,0 +1,36 @@
+// Usage:
+// console.log(tap([{ok:true, prefix: "Testing the foo module", name:"the module exists"}
+//                 ,{ok:false, name:"7 == 8"}
+//                 ,{ok:true, "'foo' === 'foo'"}]))
+
+
+
+var taps = ["Tests for the foo module"
+           ,{ok:true, name:"test that the foo is fooish"
+            ,file:"foo.js", line:8, name:"fooish test"
+            ,stack:new Error("fooish").stack}
+           ,{ok:false, name:"a test that the bar is barish"
+            ,file:"bar.js", line:25
+            ,expected:"bar\nbar\nbaz", actual:"rab\nrib\nzib"
+            ,hash:{more:"\nstuff\nhere\n",regexp:/asdf/}}
+           ,"Quux module tests"
+           ,"This is a longer comment"
+           ,{ok:true, name:"an easy one."}
+           ,{ok:false, name:"bloooooo"
+            ,expected:"blerggeyyy"
+            ,actual:"blorggeyy"}
+           ,{ok:false, name:"array test"
+            ,expected:[{ok:true},{ok:true},{stack:new Error().stack}]
+            ,actual:[1234567890,123456789,{error:new Error("yikes")}]}
+           ,{ok:true, name:"nulltest"
+            ,expected:undefined, actual:null}
+           ,{ok:true, name:"weird key test"
+            ,expected:"weird key"
+            ,actual:"weird key"
+            ,"this object":{"has a ":"weird key"
+                           ,"and a looooooooonnnnnnnnnggg":"jacket"}}
+           ,{ok:true, name:"regexp test"
+            ,regexp:/asdf/,function:function (a,b) { return a + b }}
+           ]
+
+throw "fail, yo!"
