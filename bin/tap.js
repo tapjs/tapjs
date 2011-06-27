1047053e1f87448b08f44ed31f94887a3a9953d4
isaacs <i@izs.me>
2011-06-27 11:48:20 -0700

    More humane output when running a lot of passing tests

--- bin/tap.js
+++ bin/tap.js
@@ -5,8 +5,40 @@ var argv = process.argv.slice(2)
   , dir = path.resolve(argv[0])
   , Runner = require("tap-runner")
   , r = new Runner(dir)
+  , TapProducer = require("tap-producer")
 
-r.pipe(process.stdout)
+if (process.env.TAP || process.env.TAP_DIAG) {
+  r.pipe(process.stdout)
+} else {
+  r.on("file", function (file, results, details) {
+    var s = (details.ok ? "" : "not ") + "ok "+results.name
+      , n = details.pass + "/" + details.testsTotal
+      , dots = new Array(30 - s.length - n.length).join(".")
+    console.log("%s %s %s", s, dots, n)
+    if (details.ok) {
+      if (details.skip) {
+        console.log("  skipped: %s", details.skipTotal)
+      }
+    } else {
+      // console.error(details)
+      console.log("    Command: %s", results.command)
+      console.log("    " + TapProducer.encode(details.list)
+                  .split(/\n/).join("\n    "))
+    }
+  })
+  r.on("end", function () {
+    //console.log(r)
+    var s = "total"
+      , n = r.results.pass + "/" + r.results.testsTotal
+      , dots = new Array(30 - s.length - n.length).join(".")
+      , ok = r.results.ok ? "ok" : "not ok"
+    console.log("%s %s %s\n\n%s", s, dots, n, ok)
+    process.stdout.flush()
+  })
+}
+
+
+
 r.on("end", function () {
   process.exit(r.results.tests - r.results.pass)
 })
