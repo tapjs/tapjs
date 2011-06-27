4ef29620bb9aee25667f07a1ce976f01736850a4
isaacs <i@izs.me>
2011-06-27 11:50:26 -0700

    Need to handle longer filenames

--- bin/tap.js
+++ bin/tap.js
@@ -13,7 +13,7 @@ if (process.env.TAP || process.env.TAP_DIAG) {
   r.on("file", function (file, results, details) {
     var s = (details.ok ? "" : "not ") + "ok "+results.name
       , n = details.pass + "/" + details.testsTotal
-      , dots = new Array(30 - s.length - n.length).join(".")
+      , dots = new Array(Math.max(1, 40 - s.length - n.length)).join(".")
     console.log("%s %s %s", s, dots, n)
     if (details.ok) {
       if (details.skip) {
@@ -30,7 +30,7 @@ if (process.env.TAP || process.env.TAP_DIAG) {
     //console.log(r)
     var s = "total"
       , n = r.results.pass + "/" + r.results.testsTotal
-      , dots = new Array(30 - s.length - n.length).join(".")
+      , dots = new Array(40 - s.length - n.length).join(".")
       , ok = r.results.ok ? "ok" : "not ok"
     console.log("%s %s %s\n\n%s", s, dots, n, ok)
     process.stdout.flush()
