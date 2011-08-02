601a3baf98c374e3904069e77d8304faa6d646d4
isaacs <i@izs.me>
2011-08-02 15:46:33 -0700

    Testing test failures work as expected

--- test/meta-test.js
+++ test/meta-test.js
@@ -1,7 +1,7 @@
 var tap = require("../")
   , test = tap.test
 
-test("meta success", { skip: false }, function (t) {
+test("meta test", { skip: false }, function (t) {
 
   // this also tests the ok/notOk functions
   t.once("end", section2)
@@ -17,16 +17,22 @@ test("meta success", { skip: false }, function (t) {
   t.notOk(undefined, "undefined is notOk")
   t.notOk(NaN, "NaN is notOk")
   t.notOk("", "empty string is notOk")
+
+  // a few failures.
+  t.ifError(new Error("this is an error"))
+  t.ifError({ message: "this is a custom error" })
+  t.ok(false, "false is not ok")
+  t.notOk(true, "true is not not ok")
   t.end()
 
   function section2 () {
     var results = t.results
     t.clear()
-    t.ok(true, "ok")
-    t.ok(results.ok, "ok")
-    t.equal(results.tests, 12, "12 tests.")
-    t.equal(results.passTotal, 12, "12 pass total")
-    t.equal(results.fail, 0, "0 fail")
+    t.ok(true, "sanity check")
+    t.notOk(results.ok, "not ok")
+    t.equal(results.tests, 16, "total test count")
+    t.equal(results.passTotal, 12, "tests passed")
+    t.equal(results.fail, 4, "tests failed")
     t.type(results.ok, "boolean", "ok is boolean")
     t.type(results.skip, "number", "skip is number")
     t.type(results, "Results", "results isa Results")
