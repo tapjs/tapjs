18c2d0376c402757b5a180406be7d3945606a9c4
isaacs <i@izs.me>
2011-10-17 16:06:39 -0700

    cleanup a little

--- bin/tap-http.js
+++ bin/tap-http.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
 
 // just an example, really
+// Run with `node tap-http.js path/to/tests/`
 
 var argv = process.argv.slice(2)
   , path = require("path")
   , Runner = require("tap-runner")
-  , TapProducer = require("tap-producer")
 
   , http = require("http")
   , server = http.createServer(function (req, res) {
-  // it'd be nice to return a non-200 if the tests fail, but we don't
-  // know the status until it's done, so that would mean not being able
-  // to pipe the output
-  res.writeHead(200, {'content-type': 'text/plain'})
-  var r = new Runner(argv, null)
-  r.pipe(res)
-})
+    // it'd be nice to return a non-200 if the tests fail, but we don't
+    // know the status until it's done, so that would mean not being able
+    // to pipe the output
+    res.writeHead(200, {'content-type': 'text/plain'})
+    new Runner(argv, null).pipe(res)
+  })
 
 server.listen(1337)
