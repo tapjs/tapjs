e382d3a1ff511a78ba4265eacfddc82513efb270
Pedro P. Candel <kusorbox@gmail.com>
2011-11-29 18:00:13 +0100

    Fix #20 Add relative path to test files to output.

--- bin/tap.js
+++ bin/tap.js
@@ -12,7 +12,7 @@ if (process.env.TAP || process.env.TAP_DIAG) {
   r.on("file", function (file, results, details) {
     var s = (details.ok ? "" : "not ") + "ok "+results.name
       , n = details.pass + "/" + details.testsTotal
-      , dots = new Array(Math.max(1, 40 - s.length - n.length)).join(".")
+      , dots = new Array(Math.max(1, 60 - s.length - n.length)).join(".")
     console.log("%s %s %s", s, dots, n)
     if (details.ok) {
       if (details.skip) {
@@ -29,7 +29,7 @@ if (process.env.TAP || process.env.TAP_DIAG) {
     //console.log(r)
     var s = "total"
       , n = r.results.pass + "/" + r.results.testsTotal
-      , dots = new Array(40 - s.length - n.length).join(".")
+      , dots = new Array(60 - s.length - n.length).join(".")
       , ok = r.results.ok ? "ok" : "not ok"
     console.log("%s %s %s\n\n%s", s, dots, n, ok)
     // process.stdout.flush()
