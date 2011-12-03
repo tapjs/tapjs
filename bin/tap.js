ef4cea4863f9fb9412f052f08902104bf65783cc
isaacs <i@izs.me>
2011-12-03 12:05:30 -0800

    Output coverage dir on end

--- bin/tap.js
+++ bin/tap.js
@@ -32,6 +32,10 @@ if (process.env.TAP || process.env.TAP_DIAG) {
       , dots = new Array(60 - s.length - n.length).join(".")
       , ok = r.results.ok ? "ok" : "not ok"
     console.log("%s %s %s\n\n%s", s, dots, n, ok)
+    if (r.coverageOutDir) {
+      console.error( "\nCoverage: %s\n"
+                   , path.resolve(r.coverageOutDir, "index.html") )
+    }
     // process.stdout.flush()
   })
 }
