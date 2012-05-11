c972ae8e52e4a4997718810f050244d88c1394ea
James Halliday <mail@substack.net>
2012-05-11 04:52:38 -0700

    same and notSame for failing buffer test

--- test/buffer_compare.js
+++ test/buffer_compare.js
@@ -1,11 +1,11 @@
 var test = require("../").test
 
-test("similar buffers", function (t) {
-  t.similar(new Buffer([3,4,243]), new Buffer([3,4,243]))
+test("same buffers", function (t) {
+  t.same(new Buffer([3,4,243]), new Buffer([3,4,243]))
   t.end()
 })
 
-test("dissimilar buffers", function (t) {
-  t.dissimilar(new Buffer([3,5,243]), new Buffer([3,4,243]))
+test("not same buffers", function (t) {
+  t.notSame(new Buffer([3,5,243]), new Buffer([3,4,243]))
   t.end()
 })
