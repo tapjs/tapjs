cc0adfb55434528fee81716019dc7ed19f6f10a4
James Halliday <mail@substack.net>
2012-05-11 04:31:17 -0700

    similar and dissimilar buffer tests

--- test/buffer_compare.js
+++ test/buffer_compare.js
@@ -1,6 +1,11 @@
 var test = require("../").test
 
-test("compare two buffers", function (t) {
-  t.same(new Buffer([3,4,243]), new Buffer([3,4,243]))
+test("similar buffers", function (t) {
+  t.similar(new Buffer([3,4,243]), new Buffer([3,4,243]))
+  t.end()
+})
+
+test("dissimilar buffers", function (t) {
+  t.dissimilar(new Buffer([3,5,243]), new Buffer([3,4,243]))
   t.end()
 })
