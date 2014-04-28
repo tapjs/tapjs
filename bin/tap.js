1f63b896c6c7e1c7c2010b856ef94c8621faecfb
isaacs <i@izs.me>
2014-04-28 10:53:11 -0700

    passthrough stderr by default

--- bin/tap.js
+++ bin/tap.js
@@ -43,7 +43,7 @@ var argv = process.argv.slice(2)
   , defaults =
     { cover: "./lib"
     , "cover-dir": "./coverage"
-    , stderr: process.env.TAP_STDERR || process.env.TAP
+    , stderr: true
     , tap: process.env.TAP
     , diag: process.env.TAP_DIAG
     , timeout: +process.env.TAP_TIMEOUT || 30
