c3120030c315abbcb782aa83a7636af9426f8754
isaacs <i@izs.me>
2015-02-12 11:30:22 -0800

    Fix expose-gc test
    It wasn't actually testing anything.
    
    Close #79
    Close #104

--- test/expose-gc-test.js
+++ test/expose-gc-test.js
@@ -1,17 +1,21 @@
 var tap = require("../")
   , fs = require("fs")
   , cp = require("child_process")
+  , path = require("path")
+  , dir = path.resolve(__dirname, "..")
+  , script = path.resolve(dir, "gc-script.js")
+  , opt = { cwd: dir }
 
-fs.writeFileSync("gc-script.js", "console.log(!!global.gc)", "utf8")
+fs.writeFileSync(script, "console.error(!!global.gc)", "utf8")
 
 tap.test("gc test when the gc isn't there", function (t) {
   console.error("gc test")
   t.plan(1)
   console.error("t.plan="+t._plan)
 
-  cp.exec("../bin/tap.js ./gc-script", function (err, stdo, stde) {
+  cp.exec("bin/tap.js ./gc-script.js", opt, function (err, stdo, stde) {
     console.error("assert gc does not exist")
-    t.ok("false", stdo)
+    t.equal(stde, "false\n")
   })
 })
 
@@ -25,9 +29,9 @@ tap.test("gc test when the gc should be there", function (t) {
     t.plan(1)
     console.error("t.plan="+t._plan)
 
-    cp.exec("../bin/tap.js --gc ./gc-script", function (err, stdo, stde) {
+    cp.exec("bin/tap.js --gc ./gc-script.js", opt, function (err, stdo, stde) {
       console.error("assert gc exists")
-      t.ok("true", stdo)
+      t.equal(stde, "true\n")
     })
   })
 
@@ -36,11 +40,14 @@ tap.test("gc test when the gc should be there", function (t) {
     t.plan(1)
     console.error("t.plan="+t._plan)
 
-    cp.exec("../bin/tap.js --expose-gc ./gc-script", function (err, stdo) {
+    cp.exec("bin/tap.js --expose-gc ./gc-script.js", opt, function (err, stdo, stde) {
       console.error("assert gc exists")
-      t.ok("true", stdo)
+      t.equal(stde, "true\n")
     })
   })
 })
 
-fs.unlinkSync("gc-script.js");
+tap.test("cleanup", function (t) {
+  fs.unlinkSync(script)
+  t.end()
+});
