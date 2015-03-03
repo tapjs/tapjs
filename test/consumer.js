5c8d6c63869f1655036cc5b12e5543ac1defd5eb
isaacs <i@izs.me>
2015-03-02 18:49:41 -0800

    use standard for code formatting

--- test/consumer.js
+++ test/consumer.js
@@ -1,27 +1,5 @@
 var Consumer = require('../lib/consumer.js')
-
-function listen (consumer, ignore) {
-  ignore = ignore || []
-  var events = []
-  consumer.emit = function (orig) {
-    return function (ev) {
-      if (ignore.indexOf(ev) === -1) {
-        var l = arguments.length
-        var args = new Array(l)
-        for (var i = 0; i < l; i++) {
-          var arg = arguments[i]
-          args[i] = (arg instanceof Consumer) ?
-            listen(arg, ignore) :
-            arg
-        }
-        events.push(args)
-      }
-
-      return orig.apply(this, arguments)
-    }
-  }(consumer.emit)
-  return events
-}
+var etoa = require('events-to-array')
 
 var ignore = [ 'pipe', 'unpipe', 'prefinish', 'finish' ]
 var glob = require('glob')
@@ -39,7 +17,7 @@ glob.sync(__dirname + '/consumer/*.tap').forEach(function (tapfile) {
 
   test(path.basename(tapfile), function (t) {
     var consumer = new Consumer
-    var found = listen(consumer, ignore)
+    var found = etoa(consumer, ignore)
     consumer.on('complete', function () {
       t.same(found, wanted)
       t.end()
