3e9c5598d21d8427ad4263586f14bc2c25c94c12
isaacs <i@izs.me>
2015-03-04 23:41:11 -0800

    test for error handling, and simplify root test object

---
+++ test/errors/throws-and-plans.js
@@ -0,0 +1,77 @@
+//var t = require('./lib/root.js')
+
+//var t = require('./lib/test.js')()
+//t.pipe(process.stdout)
+
+var t = require('../../lib/test.js')()
+t.pipe(process.stdout)
+
+t._name = 'ROOT'
+
+t.ok('true')
+
+t.test('plans of 1', function (t) {
+  t.pass('before child')
+
+  t.test('sync thrower', function (tt) {
+    tt.plan(1)
+    tt.pass('before the bomb')
+
+    throw new Error('pwnSync')
+    tt.pass('after the bomb')
+    tt.end()
+  })
+
+  t.test('async thrower', function (tt) {
+    tt.plan(1)
+    tt.pass('before set the bomb')
+    setTimeout(function () {
+      tt.pass('before the bomb')
+      throw new Error('pwn')
+      tt.pass('after the bomb')
+      tt.end()
+    })
+    tt.pass('after set the bomb')
+  })
+  t.pass('after child')
+  t.end()
+})
+
+t.test('no assert only throw', function (t) {
+  var assert = require('assert')
+  assert(true, 'true is truthy')
+  assert(false, 'false is truthy right?')
+})
+
+t.test('plans of 8', function (t) {
+  t.pass('before child')
+
+  t.test('sync thrower', function (tt) {
+    tt.plan(8)
+    tt.pass('before the bomb')
+
+    throw new Error('pwnSync')
+    tt.pass('after the bomb')
+    tt.end()
+  })
+
+  //return t.end()
+
+  t.test('async thrower', function (tt) {
+    tt.plan(8)
+    tt.pass('before set the bomb')
+    setTimeout(function () {
+      tt.pass('before the bomb')
+      throw new Error('pwn')
+      tt.pass('after the bomb')
+      tt.end()
+    })
+    tt.pass('after set the bomb')
+  })
+  t.pass('after child')
+  t.end()
+})
+
+process.on('exit', function () {
+  t.end()
+})
