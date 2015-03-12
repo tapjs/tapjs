be2397a0dbd8ef93059e5c07a4a58c984cfda0f8
isaacs <i@izs.me>
2015-03-11 17:40:59 -0700

    Use tap-parser module instead of built-in consumer

--- test/consumer/giving-up.js
+++
@@ -1,7 +0,0 @@
-module.exports =
-[ [ 'version', '13' ],
-  [ 'plan', 573 ],
-  [ 'bailout', 'Couldn\'t connect to database.' ],
-  [ 'result', { id: 1, ok: false, name: 'database handle' } ],
-  [ 'complete',
-    { plan: 573, count: 1, pass: 0, ok: false, fail: 1, bailout: true } ] ]
--- test/consumer/skip-all.js
+++
@@ -1,4 +0,0 @@
-module.exports =
-[ [ 'plan', 0 ],
-  [ 'comment', '# Skipped: WWW::Mechanize not installed\n' ],
-  [ 'complete', { plan: 0, count: 0, pass: 0, ok: true } ] ]
