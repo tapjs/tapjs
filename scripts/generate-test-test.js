95f15e936ed2c84c6c17c6afa3ed7c83eb0ded62
isaacs <i@izs.me>
2015-03-14 14:21:36 -0700

    clean up t.spawn() output

--- scripts/generate-test-test.js
+++ scripts/generate-test-test.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 var spawn = require('child_process').spawn
-var node = process.execPath
 var path = require('path')
+var node = process.execPath
 var stackre = new RegExp('^\\s*\\- "')
 var fs = require('fs')
 
@@ -12,7 +12,11 @@ for (var i = 2; i < process.argv.length; i++) {
 
 function generate(file) {
   file = path.resolve(file)
-  console.error(file)
+  var f = file
+  if (f.indexOf(process.cwd()) === 0)
+    f = './' + file.substr(process.cwd().length + 1)
+
+  console.error(f)
   var outfile = file.replace(/\.js$/, '.tap')
   var output = ''
   var c = spawn(node, [file])
@@ -21,8 +25,16 @@ function generate(file) {
   })
   c.on('close', function () {
     output = output.split(file).join('{{{/.*/}}}' + path.basename(file))
-    output = output.split(node + ' {{{/').join('{{{/.*(node|iojs)')
-    output = output.split(node).join('{{{/.*(node|iojs)/}}}')
+    output = output.split(f).join('{{{/.*/}}}' + path.basename(f))
+
+    output = output.split(node + ' {{{/').join('\0N1\0')
+    output = output.split(path.basename(node) + ' {{{/').join('\0N1\0')
+
+    output = output.split(node).join('\0N2\0')
+    output = output.split(path.basename(node)).join('\0N2\0')
+
+    output = output.split('\0N1\0').join('{{{/.*(node|iojs)')
+    output = output.split('\0N2\0').join('{{{/.*(node|iojs)/}}}')
     var stacks = output.split('stack:\n')
     output = stacks.shift()
     stacks.forEach(function (stack) {
