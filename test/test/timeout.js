6cd663d84a2330e3b7be225165b773fc9b2ecf39
isaacs <i@izs.me>
2015-03-15 08:47:31 -0700

    support test timeouts

---
+++ test/test/timeout.js
@@ -0,0 +1,16 @@
+var t = require('../../lib/test.js')()
+t.pipe(process.stdout)
+process.on('exit', function () {
+  t.end()
+})
+
+t.test('timeout test', { timeout: 50 }, function (t) {
+  t.test('this never completes', function (tt) {
+    tt.ok('wait a sec...')
+    setTimeout(function () {
+      tt.pass('ok done')
+      tt.end()
+    }, 100)
+  })
+  t.end()
+})
