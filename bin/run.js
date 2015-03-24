88a7485829bb31567901657976d005f4359eda4b
isaacs <i@izs.me>
2015-03-24 16:33:10 -0700

    Don't try to run things that aren't runnable, or javascript things that aren't javascript

--- bin/run.js
+++ bin/run.js
@@ -19,6 +19,34 @@ formatted test result data.
 */}.toString().split('\n').slice(1, -1).join('\n')
 }
 
+var isExe
+if (process.platform == "win32") {
+  // On windows, there is no good way to check that a file is executable
+  isExe = function isExe () { return true }
+} else {
+  isExe = function isExe (stat) {
+    var mod = stat.mode
+    var uid = stat.uid
+    var gid = stat.gid
+    var u = parseInt('100', 8)
+    var g = parseInt('010', 8)
+    var o = parseInt('001', 8)
+    var ug = u | g
+
+    var ret = (mod & o)
+        || (mod & g) && process.getgid && gid === process.getgid()
+        || (mod & u) && process.getuid && uid === process.getuid()
+        || (mod & ug) && process.getuid && 0 === process.getuid()
+
+    return ret
+  }
+}
+
 var tap = require('../lib/root.js')
-for (var i = 0; i < args.length; i++)
-  tap.spawn(process.execPath, [ args[i] ])
+var fs = require('fs')
+for (var i = 0; i < args.length; i++) {
+  if (args[i].match(/\.js$/))
+    tap.spawn(process.execPath, [ args[i] ])
+  else if (isExe(fs.statSync(args[i])))
+    tap.spawn(args[i], [])
+}
