af131f210fa6806c8e8c9316e945f49a27dfec1b
isaacs <i@izs.me>
2015-04-30 00:26:11 -0400

    Stronger faster better timeout test

--- test/independent-timeouts.js
+++ test/independent-timeouts.js
@@ -1,16 +1,29 @@
 // https://github.com/isaacs/node-tap/issues/23
 
 var tap = require("../")
-  , test = tap.test
+var test = tap.test
+var Test = tap.Test
 
-test("finishes in time", {timeout: 500}, function(t) {
+test("finishes in time", {timeout: 100}, function(t) {
   setTimeout(function () {
     t.end();
-  }, 300);
-})
-test("finishes in time too", {timeout: 500}, function(t) {
-  setTimeout(function () {
-    t.end();
-  }, 300);
+  }, 60);
 })
 
+test("finishes in time too", {timeout: 100}, function(t) {
+  setTimeout(function () {
+    t.end();
+  }, 60);
+})
+
+test('does not finish in time', function (t) {
+  var tt = new Test()
+  tt.test('timeout', { timeout: 50 }, function (ttt) {
+    setTimeout(function () {
+      ttt.fail('shouldve timed out')
+      ttt.end()
+      t.notOk(tt._ok)
+    }, 60)
+  })
+  tt.end()
+})
