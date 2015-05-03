6ff62e1c73589f0377ff49b7333b4784f3a43850
isaacs <i@izs.me>
2015-05-02 18:01:29 -0700

    More thorough cleanup on timeout

--- test/test/timeout.js
+++ test/test/timeout.js
@@ -1,17 +1,20 @@
-var t = require('../../lib/test.js')()
-t.pipe(process.stdout)
-process.on('exit', function () {
-  t.end()
-})
+var t = require('../../lib/root.js')
 
 t.test('parent of timeout test', function (t) {
   t.test('timeout test', { timeout: 50 }, function (t) {
     t.test('this never completes', function (tt) {
-      tt.ok('wait a sec...')
-      setTimeout(function () {
-        tt.pass('ok done')
-        tt.end()
-      }, 100)
+      tt.test('baby', function (tt) {
+        tt.ok('wait a sec...')
+        setTimeout(function () {
+          tt.pass('ok done')
+          tt.end()
+        }, 1000).unref()
+      })
+      tt.pass('p')
+      tt.pass('a')
+      tt.pass('s')
+      tt.pass('s')
+      tt.end()
     })
     t.end()
   })
