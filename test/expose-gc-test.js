6dc04d2c0e872515c37061fc387593e3cd49801d
isaacs <i@izs.me>
2015-05-18 17:37:11 -0700

    Use a persistent fixture for gc-script.js test

--- test/expose-gc-test.js
+++ test/expose-gc-test.js
@@ -3,14 +3,12 @@ var fs = require("fs")
 var cp = require("child_process")
 var path = require("path")
 var dir = path.resolve(__dirname, "..")
-var script = path.resolve(dir, "gc-script.js")
+var script = require.resolve('./fixtures/gc-script.js')
 var opt = { cwd: dir }
-
-fs.writeFileSync(script, "console.error(!!global.gc)", "utf8")
 
 tap.test("gc test when the gc isn't there", function (t) {
   t.plan(1)
-  cp.exec("bin/run.js ./gc-script.js", opt, function (err, stdo, stde) {
+  cp.exec("bin/run.js test/fixtures/gc-script.js", opt, function (err, stdo, stde) {
     t.equal(stde, "false\n")
   })
 })
@@ -20,7 +18,7 @@ tap.test("gc test when the gc should be there", function (t) {
   t.test("test for gc using --gc", function (t) {
     t.plan(1)
 
-    cp.exec("bin/run.js --gc ./gc-script.js", opt, function (err, stdo, stde) {
+    cp.exec("bin/run.js --gc test/fixtures/gc-script.js", opt, function (err, stdo, stde) {
       t.equal(stde, "true\n")
     })
   })
@@ -28,13 +26,8 @@ tap.test("gc test when the gc should be there", function (t) {
   t.test("test for gc using --expose-gc", function (t) {
     t.plan(1)
 
-    cp.exec("bin/run.js --expose-gc ./gc-script.js", opt, function (err, stdo, stde) {
+    cp.exec("bin/run.js --expose-gc test/fixtures/gc-script.js", opt, function (err, stdo, stde) {
       t.equal(stde, "true\n")
     })
   })
 })
-
-tap.test("cleanup", function (t) {
-  fs.unlinkSync(script)
-  t.end()
-});
---
+++ test/fixtures/gc-script.js
@@ -0,0 +1,1 @@
+console.error(!!global.gc)
