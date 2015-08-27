7fa8df4dfc6b010f7f1affebe9a37633a250a190
isaacs <i@izs.me>
2015-08-26 18:28:17 -0700

    Fail on unfinished tests
    Fix #161
    Fix #152
    Re #129
    
    This is a pretty significant change.  It may introduce some annoying
    test failures in some cases, but most likely, those are tests that were
    silently being ignored before.
    
    Now, when `t.endAll()` is called, if there's anything waiting in the
    queue, it will be treated as a failure, and reported with extreme
    prejudice.  It should be virtually impossible after this change to ever
    have a test simply be ignored or have the tap stream cut off after
    starting a new child test.

--- test/test/unfinished.js
+++ test/test/unfinished.js
@@ -1,16 +1,34 @@
-var tap = require('../..')
+var tap = require('../..');
 
-tap.test('parent', function(t) {
-  t.test('child 1', function(t) {
-    t.test('grandchild 1', function(t) {
-      t.pass('1')
-      t.end()
-    })
+tap.test('t1', function(t) {
+  t.test('t11', function (t) {
+    process.on('some event that never happens', function() {
+      t.pass('ok');
+    });
   })
-  t.test('child 2', function(t) {
-    t.test('grandchild 2', function(t) {
-      t.pass('2')
-      t.end()
-    })
-  })
-})
+  t.end()
+});
+
+tap.equal(1, 1, '1 === 1')
+tap.ok('this is ok')
+tap.fail('failsome', { hoo: 'hah' })
+
+tap.spawn('node', [__filename], {}, 'spawny', { rar: 'grr' })
+
+tap.test(function(t) {
+  process.nextTick(function() {
+    t.end();
+  });
+});
+
+tap.test('', function(t) {
+  process.nextTick(function() {
+    t.end();
+  });
+});
+
+tap.test('t2', function(t) {
+  process.nextTick(function() {
+    t.end();
+  });
+});
