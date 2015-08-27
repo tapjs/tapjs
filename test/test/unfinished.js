d94434ebfc6b29421435ba4b1029284609f4022c
isaacs <i@izs.me>
2015-08-27 16:15:19 -0700

    Treat un-met plans as unfinished tests, add test name to failure

--- test/test/unfinished.js
+++ test/test/unfinished.js
@@ -2,10 +2,19 @@ var tap = require('../..');
 
 tap.test('t1', function(t) {
   t.test('t11', function (t) {
+    t.plan(1)
     process.on('some event that never happens', function() {
       t.pass('ok');
     });
   })
+
+  // Nothing beyond this point will actually be processed
+  // Everything will just get stuffed into the queue, because
+  // the child test above is unfinished.  When the process
+  // ends, because there's no pending IO, it'll emit a bunch of
+  // failures indicating that they were abandoned in the queue.
+
+  t.ok(true, 'this would be ok if it ever happened')
   t.end()
 });
 
@@ -14,21 +23,16 @@ tap.ok('this is ok')
 tap.fail('failsome', { hoo: 'hah' })
 
 tap.spawn('node', [__filename], {}, 'spawny', { rar: 'grr' })
+tap.spawn('node', ['--version'], {}, '', { rar: 'grr' })
 
 tap.test(function(t) {
-  process.nextTick(function() {
-    t.end();
-  });
-});
+  process.nextTick(t.end)
+})
 
 tap.test('', function(t) {
-  process.nextTick(function() {
-    t.end();
-  });
-});
+  process.nextTick(t.end)
+})
 
 tap.test('t2', function(t) {
-  process.nextTick(function() {
-    t.end();
-  });
+  process.nextTick(t.end)
 });
