1b64687c8c1e5286fce8f20754f5b4444c27a00a
isaacs <i@izs.me>
2015-09-21 00:17:07 -0700

    Generate test tests JSON such that arrays are preserved
    Also, fix pending-handles test so order is not ambiguous

--- test/test/pending-handles.js
+++ test/test/pending-handles.js
@@ -2,20 +2,42 @@ var t = require('../..')
 
 if (process.argv[2] === 'child') {
   t.pass('this is ok')
-  setInterval(function () {}, 100000)
 
-  var fs = require('fs')
-  fs.open(__filename, 'r', function onOpen (er, fd) {
-    fs.open(__filename, 'r', onOpen)
-    if (fd) fs.closeSync(fd)
-  })
+  switch (process.argv[3]) {
+  case 'timer':
+    setInterval(function () {}, 100000)
+    break
 
-  var server = require('http').createServer(function (req, res) {
-  }).listen(8000, function () {
-    var req = require('http').request('http://localhost:8000/foo').end()
-  })
+  case 'fs':
+    var fs = require('fs')
+    fs.open(__filename, 'r', function onOpen (er, fd) {
+      fs.open(__filename, 'r', onOpen)
+      if (fd) fs.closeSync(fd)
+    })
+    break
 
+  case 'server':
+    var http = require('http')
+    var server = http.createServer(function (req, res) {
+      // hang
+    }).listen(8000, function () {
+      var req = require('http').request(
+        'http://localhost:8000/foo').end()
+    })
+    break
+
+  }
   return
 }
 
-t.spawn(process.execPath, [__filename, 'child'], {}, '', { timeout: 200 })
+var args = [
+  'timer',
+  'server',
+  'fs'
+]
+
+args.forEach(function (a) {
+  t.spawn(process.execPath, [__filename, 'child', a], {}, '', {
+    timeout: 500
+  })
+})
