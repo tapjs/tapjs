ee52ee4fc0cd000f3a772bdb4b7571a5efb2df52
isaacs <i@izs.me>
2015-09-23 23:33:52 -0700

    remove unused fixture

--- test/fixtures/todo-skip-descriptions.js
+++
@@ -1,4 +0,0 @@
-console.log('ok - always passes # SKIP skip it, skip it good')
-console.log('not ok - false # SKIP always fails')
-console.log('ok - bonus # TODO remove todo directive')
-console.log('not ok - expected # TODO implement a thing')
