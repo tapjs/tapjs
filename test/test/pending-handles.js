1999f774ce5c6110792f424637e088c12d1e5169
isaacs <i@izs.me>
2015-09-24 10:08:42 -0700

    pending handles test: remove server/fs, too inconsistent on Travis

--- test/test/pending-handles.js
+++ test/test/pending-handles.js
@@ -2,42 +2,10 @@ var t = require('../..')
 
 if (process.argv[2] === 'child') {
   t.pass('this is ok')
-
-  switch (process.argv[3]) {
-  case 'timer':
-    setInterval(function () {}, 100000)
-    break
-
-  case 'fs':
-    var fs = require('fs')
-    fs.open(__filename, 'r', function onOpen (er, fd) {
-      fs.open(__filename, 'r', onOpen)
-      if (fd) fs.closeSync(fd)
-    })
-    break
-
-  case 'server':
-    var http = require('http')
-    var server = http.createServer(function (req, res) {
-      // hang
-    }).listen(8000, function () {
-      var req = require('http').request(
-        'http://localhost:8000/foo').end()
-    })
-    break
-
-  }
+  setInterval(function () {}, 100000)
   return
 }
 
-var args = [
-  'timer',
-  'server',
-  'fs'
-]
-
-args.forEach(function (a) {
-  t.spawn(process.execPath, [__filename, 'child', a], {}, '', {
-    timeout: 900
-  })
+t.spawn(process.execPath, [__filename, 'child'], {}, '', {
+  timeout: 900
 })
