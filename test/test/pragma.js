1945968e327a7b39e89087b5ea4d8d140ff6768b
isaacs <i@izs.me>
2016-06-05 01:43:24 -0700

    Just a few tests to push up the coverage a bit

--- test/test/pragma.js
+++ test/test/pragma.js
@@ -1,8 +1,14 @@
 var t = require('../..')
 
 if (process.argv[2] !== 'child') {
+  // +child does nothing, just verifying that it gets delayed
+  t.pragma({ child: true })
   t.spawn(process.execPath, [__filename, 'child'])
-} else {
+  t.pragma({ child: false })
+  t.spawn(process.execPath, [__filename, 'child', 'bailout'])
+  // this one should be skipped entirely
+  t.pragma({ bailedout: true })
+} else if (process.argv[3] !== 'bailout') {
   console.log('this is not tap but it is ok')
   t.pass('an ok test')
   t.pragma({ strict: true })
@@ -11,4 +17,8 @@ if (process.argv[2] !== 'child') {
   console.log('more ok non-tap')
   t.pass('ending now')
   t.end()
+} else {
+  t.removeAllListeners('bailout')
+  t.bailout('no pragmas come after bailout')
+  t.pragma({bailout: true })
 }
