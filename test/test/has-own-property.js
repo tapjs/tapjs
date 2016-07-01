8b86ed38b6abf74b41880f0d2de80d7d75ce7768
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2016-07-01 04:12:43 +0200

    use `Object.keys(...).forEach(...` everywhere instead of `hasOwnProperty(...)`

--- test/test/has-own-property.js
+++ test/test/has-own-property.js
@@ -1,14 +1,14 @@
 'use strict'
 
-var tap = require('../..')
-
 Object.prototype[ 'Answer to the Ultimate Question of Life, the Universe, and Everything' ] = 42
 
+var tap = require('../..')
+
 function error() {
-    throw new Error('fake')
+  throw new Error('fake')
 }
 
 tap.test('obj.hasOwnProperty(key) checks', function (test) {
-    test.throws(error, 'this fails without internal checks')
-    test.end()
+  test.throws(error, 'this fails without internal checks')
+  test.end()
 })
