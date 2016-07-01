1f88f4a58ef4d8bed3e602e1d4c592958b8e3302
isaacs <i@izs.me>
2016-06-30 18:53:18 -0700

    Child tests should inherit bail-on-fail from parent
    Unless explicitly specified in options object.

--- test/throw-after-end.js
+++ test/throw-after-end.js
@@ -1,7 +1,8 @@
 var t = require('../')
 var Test = t.Test
 
-var tt = new Test()
+var tt = new Test({ bail: false })
+tt._name = 'one'
 tt.pass('this is fine')
 tt.end()
 t.throws(function () {
@@ -9,15 +10,18 @@ t.throws(function () {
 }, { message: 'whoops', stack: /^Error: whoops\n/ })
 
 var out = ''
-tt = new Test()
+tt = new Test({ bail: false })
+tt._name = 'two'
+
 tt.on('data', function (c) {
   out += c
 })
-tt.test(function (tt) {
+tt.test('child', function (tt) {
   tt.threw(new Error('whoops'))
   tt.end()
 })
 tt.end()
+
 t.match(out, '\n    not ok 1 - Error: whoops\n')
 t.match(out, '\n        tt.threw(new Error(\'whoops\'))\n')
-t.match(out, '\nnot ok 1 - (unnamed test) # time=')
+t.match(out, '\nnot ok 1 - child # time=')
