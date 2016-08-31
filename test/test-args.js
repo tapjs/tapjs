e775e699785ef9d186e7bd76ea83f13fac236d28
isaacs <i@izs.me>
2016-08-31 10:09:37 -0700

    Newer V8 names functions if they're assigned to a variable

--- test/test-args.js
+++ test/test-args.js
@@ -9,21 +9,21 @@ function runTest (args, expect) {
   delete obj.todo
 
   var result = t._parseTestArgs.apply(t, args)
-  t.equal(result[0], expect[0])
+  t.match(result[0], expect[0])
   t.same(result[1], expect[1])
   t.equal(result[2], expect[2])
 }
 
 runTest(['name', obj, fn], ['name', obj, fn])
 runTest(['name', fn], ['name', {}, fn])
-runTest([obj, fn], ['(unnamed test)', obj, fn])
+runTest([obj, fn], [/\(unnamed test\)|fn/, obj, fn])
 runTest([obj, namedFunction], ['namedFunction', obj, namedFunction])
 runTest(['name', obj], ['name', objTodo])
 runTest(['name'], ['name', { todo: true }])
-runTest([obj], ['(unnamed test)', objTodo])
-runTest([fn], ['(unnamed test)', {}, fn])
+runTest([obj], [/\(unnamed test\)|fn/, objTodo])
+runTest([fn], [/\(unnamed test\)|fn/, {}, fn])
 runTest([namedFunction], ['namedFunction', {}, namedFunction])
-runTest([], ['(unnamed test)', { todo: true }])
+runTest([], [/\(unnamed test\)|fn/, { todo: true }])
 
 t.throws(function () {
   runTest(['name', obj, 99], [])
