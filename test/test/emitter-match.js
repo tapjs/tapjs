81aa9138689395e2a1d53ade72ddbae7f5c94bbb
isaacs <i@izs.me>
2017-04-23 16:31:43 -0700

    Fix tests on 0.10

--- test/test/emitter-match.js
+++ test/test/emitter-match.js
@@ -3,6 +3,9 @@ var EE = require('events').EventEmitter
 t.test('check ee matches', function (t) {
   var e = new EE()
   e.truthy = false
+  // put this here for 0.10's benefit
+  e._eventsCount = 0
+  e._maxListeners = 10
   t.match(e, { truthy: true })
   t.end()
 })
