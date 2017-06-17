56a4a8ee1f9432885c612856b12fc1c7a4704fe3
Gregor <gregor@martynus.net>
2017-06-16 18:10:27 -0700

    add test for `--only` flag

---
+++ test/test/only.js
@@ -0,0 +1,11 @@
+var test = require('../..').test
+
+test('normal', function (t) {
+  t.pass('will log an error when run without TAP_ONLY=1')
+  t.end()
+})
+
+test('only', {skip: true}, function (t) {
+  t.pass('ok')
+  t.end()
+})
