8728487cb2571248c9e6d6dae6806fa6b2aa5224
Gregor <gregor@martynus.net>
2017-06-21 11:31:36 -0400

    fixup! runner test for --only

--- test/fixtures/only.js
+++ test/fixtures/only.js
@@ -1,11 +1,6 @@
 var test = require('../..').test
 
-test('normal', function (t) {
-  t.pass('will log an error when run without TAP_ONLY=1')
-  t.end()
-})
-
 test('only', {only: true}, function (t) {
-  t.pass('ok')
+  t.pass('will log an error when run without TAP_ONLY=1')
   t.end()
 })
--- test/runner-only.js
+++ test/runner-only.js
@@ -3,7 +3,7 @@ var execFile = require('child_process').execFile
 var node = process.execPath
 var run = require.resolve('../bin/run.js')
 
-t.test('--only', function (t) {
+t.test('--only flag', function (t) {
   var file = require.resolve('./fixtures/only.js')
   var args = [
     run,
@@ -15,7 +15,27 @@ t.test('--only', function (t) {
     if (err) {
       throw err
     }
+
     t.equal(stderr, '')
+
+    t.match(stdout, /^ok 1 - .*[\\\/]only.js/m)
+    t.end()
+  })
+})
+
+t.test('{only: true} without --only flag', function (t) {
+  var file = require.resolve('./fixtures/only.js')
+  var args = [
+    run,
+    file
+  ]
+
+  execFile(node, args, function (err, stdout, stderr) {
+    if (err) {
+      throw err
+    }
+
+    t.equal(stderr.trim(), '# Warning: "only" has {only: true} set but all tests are run')
 
     t.match(stdout, /^ok 1 - .*[\\\/]only.js/m)
     t.end()
