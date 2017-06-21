87b7b50893d54c5f9a81ea6654c176acf90f4300
Gregor <gregor@martynus.net>
2017-06-21 12:15:04 -0400

    WIP test/fixtures/only.js with subtests

--- test/fixtures/only.js
+++ test/fixtures/only.js
@@ -1,6 +1,25 @@
 var test = require('../..').test
 
-test('only', {only: true}, function (t) {
-  t.pass('will log an error when run without TAP_ONLY=1')
+test('root test without {only: true}', function (t) {
+  t.pass('test will not run without --only')
   t.end()
+})
+
+test('root test with {only: true}', {only: true}, function (t) {
+  t.pass('test will log an error when run without --only')
+  t.end()
+})
+
+test('group', function (group) {
+  group.test('subtest without {only: true}', function (t) {
+    t.pass('subtest will not run without --only')
+    t.end()
+  })
+
+  group.test('subtest with {only: true}', {only: true}, function (t) {
+    t.pass('test will log an error when run without --only')
+    t.end()
+  })
+
+  group.end()
 })
