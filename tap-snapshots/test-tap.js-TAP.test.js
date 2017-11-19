1e86c33bdb7557ed921d56ecdc3142cb6647b432
isaacs <i@izs.me>
2017-11-19 12:30:45 -0800

    Include a banner at the top of snapshot files encouraging good behavior

--- tap-snapshots/test-tap.js-TAP.test.js
+++ tap-snapshots/test-tap.js-TAP.test.js
@@ -1,3 +1,9 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
 'use strict'
 exports[`test/tap.js TAP ok > exit status 1`] = `
 { code: 0, signal: null }
