b300e348cc80ac8d7caec38757de21d6c4420dc7
isaacs <i@izs.me>
2019-01-26 16:44:43 -0800

    no need to export types

--- index.js
+++ index.js
@@ -2,8 +2,4 @@
 module.exports = {
   parse: require('./lib/parse.js'),
   stringify: require('./lib/stringify.js'),
-  types: {
-    symbolType: require('./lib/types/symbol.js'),
-    functionType: require('./lib/types/function.js'),
-  },
 }
