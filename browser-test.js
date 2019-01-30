7daf0181fdd6d98b2618bbaf3565ddf3aa6c95e1
isaacs <i@izs.me>
2019-01-30 11:00:13 -0800

    do not run browser test in node v6

--- browser-test.js
+++ browser-test.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-if (process.platform !== 'win32') {
+if (process.platform === 'win32') {
+  console.log('browser test not supported on windows')
+} else if (process.version.match(/^v[0-6]\./)) {
+  console.log('browser test not suppored prior to node v8')
+} else if (process.platform !== 'win32') {
   console.error('> karma start karma.conf.js')
   require('child_process').spawn('karma', ['start', 'karma.conf.js'], {
     stdio: 'inherit'
@@ -8,5 +12,4 @@ if (process.platform !== 'win32') {
     if (code || signal)
       process.exit(code || 1)
   })
-} else
-  console.log('browser test not supported on windows')
+}
