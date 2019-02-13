e76bf53abb0034efa2fa357303413116a810a1d9
isaacs <i@izs.me>
2019-02-12 22:42:42 -0800

    first test, found first bug

---
+++ tap-snapshots/test-index.js-TAP.test.js
@@ -0,0 +1,160 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/index.js TAP > undefined 1`] = `
+Object {
+  "a": 1,
+}
+`
+
+exports[`test/index.js TAP > undefined 2`] = `
+{ diff: '--- actual\\n+++ expected\\n Object {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 3`] = `
+{ diff: '--- actual\\n+++ expected\\n Object {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 4`] = `
+{ diff: '--- actual\\n+++ expected\\n Object {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 5`] = `
+{ diff: '--- actual\\n+++ expected\\n Object {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 6`] = `
+{ diff: '--- actual\\n+++ expected\\n Object {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 7`] = `
+{
+  "a": 1,
+}
+`
+
+exports[`test/index.js TAP > undefined 8`] = `
+{ diff: '--- actual\\n+++ expected\\n {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 9`] = `
+{ diff: '--- actual\\n+++ expected\\n {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 10`] = `
+{ diff: '--- actual\\n+++ expected\\n {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 11`] = `
+{ diff: '--- actual\\n+++ expected\\n {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 12`] = `
+{ diff: '--- actual\\n+++ expected\\n {\\n   "a": 1,\\n }',
+  match: true }
+`
+
+exports[`test/index.js TAP > undefined 13`] = `
+Object {
+  "a": 1,
+}
+`
+
+exports[`test/index.js TAP > undefined 14`] = `
+--- actual
++++ expected
+ Object {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 15`] = `
+--- actual
++++ expected
+ Object {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 16`] = `
+--- actual
++++ expected
+ Object {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 17`] = `
+--- actual
++++ expected
+ Object {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 18`] = `
+--- actual
++++ expected
+ Object {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 19`] = `
+{
+  "a": 1,
+}
+`
+
+exports[`test/index.js TAP > undefined 20`] = `
+--- actual
++++ expected
+ {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 21`] = `
+--- actual
++++ expected
+ {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 22`] = `
+--- actual
++++ expected
+ {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 23`] = `
+--- actual
++++ expected
+ {
+   "a": 1,
+ }
+`
+
+exports[`test/index.js TAP > undefined 24`] = `
+--- actual
++++ expected
+ {
+   "a": 1,
+ }
+`
---
+++ test/index.js
@@ -0,0 +1,33 @@
+const t = require('tap')
+const compare = require('../')
+
+// not much of a test, but just verifying the interface exists
+const obj = { a: 1 }
+const expect = { a: 1 }
+const opt = { style: 'js' }
+
+t.matchSnapshot(compare.format(obj))
+t.matchSnapshot(compare.same(obj, expect))
+t.matchSnapshot(compare.strict(obj, expect))
+t.matchSnapshot(compare.has(obj, expect))
+t.matchSnapshot(compare.hasStrict(obj, expect))
+t.matchSnapshot(compare.match(obj, expect))
+t.matchSnapshot(compare.format(obj, opt))
+t.matchSnapshot(compare.same(obj, expect, opt))
+t.matchSnapshot(compare.strict(obj, expect, opt))
+t.matchSnapshot(compare.has(obj, expect, opt))
+t.matchSnapshot(compare.hasStrict(obj, expect, opt))
+t.matchSnapshot(compare.match(obj, expect, opt))
+
+t.matchSnapshot(new compare.Format(obj).print())
+t.matchSnapshot(new compare.Same(obj, { expect }).print())
+t.matchSnapshot(new compare.Strict(obj, { expect }).print())
+t.matchSnapshot(new compare.Has(obj, { expect }).print())
+t.matchSnapshot(new compare.HasStrict(obj, { expect }).print())
+t.matchSnapshot(new compare.Match(obj, { expect }).print())
+t.matchSnapshot(new compare.Format(obj, opt).print())
+t.matchSnapshot(new compare.Same(obj, { expect, ...opt }).print())
+t.matchSnapshot(new compare.Strict(obj, { expect, ...opt }).print())
+t.matchSnapshot(new compare.Has(obj, { expect, ...opt }).print())
+t.matchSnapshot(new compare.HasStrict(obj, { expect, ...opt }).print())
+t.matchSnapshot(new compare.Match(obj, { expect, ...opt }).print())
