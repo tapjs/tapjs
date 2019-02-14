bf8bd988e1631108793e9a5643816c45db628ad4
isaacs <i@izs.me>
2019-02-14 00:46:11 -0800

    test for has, and bugs found

---
+++ tap-snapshots/test-has.js-TAP.test.js
@@ -0,0 +1,419 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/has.js TAP > undefined 1`] = `
+--- actual
++++ expected
+ Object {
+   "i": Object [
+     1,
+     2,
+     3,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 2`] = `
+--- actual
++++ expected
+ Object {
+   "i": Object [
+     1,
+     2,
+     3,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 3`] = `
+--- actual
++++ expected
+ Object {
+   "i": Object [
+     1,
+     2,
+     3,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 4`] = `
+--- actual
++++ expected
+ Object {
+   "a": Array [
+     1,
+     2,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 5`] = `
+--- actual
++++ expected
+ Object {
+   "i": Object [
+     1,
+     2,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 6`] = `
+--- actual
++++ expected
+ Object {
+   "m": Map {
+     1 => 2,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 7`] = `
+--- actual
++++ expected
+ Object {
+   "m": Map {
+     3 => 4,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 8`] = `
+--- actual
++++ expected
+ Object {
+   "m": Map {
+     3 => 4,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 9`] = `
+--- actual
++++ expected
+ Object {
+   "cm": Map {
+     Object {
+       "a": 1,
+     } => 1,
+     Object {
+       "a": 1,
+     } => 1,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 10`] = `
+--- actual
++++ expected
+ Object {
+   "m": Map {
++    1 => 2,
+     3 => 4,
+-    "3" => 4,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 11`] = `
+--- actual
++++ expected
+ Object {
+   "m": Map {
+     1 => 2,
+     3 => 4,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 12`] = `
+--- actual
++++ expected
+ Object {
+   "m": Map {
+     1 => 2,
+     3 => 4,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 13`] = `
+--- actual
++++ expected
+ Object {
+   "m": Map {
+     1 => 2,
++    3 => 4,
+-    4 => 3,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 14`] = `
+--- actual
++++ expected
+ Object {
+   "em": Map {
+-    1 => 2,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 15`] = `
+--- actual
++++ expected
+ Object {
+   "em": Map {
+-    1 => 2,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 16`] = `
+--- actual
++++ expected
+ Object {
+   "em": Map {},
+ }
+`
+
+exports[`test/has.js TAP > undefined 17`] = `
+--- actual
++++ expected
+ Object {
+   "s": Set {
+     3,
+     2,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 18`] = `
+--- actual
++++ expected
+ Object {
+   "es": Set {},
+ }
+`
+
+exports[`test/has.js TAP > undefined 19`] = `
+--- actual
++++ expected
+ Object {
+   "s": Set {},
+ }
+`
+
+exports[`test/has.js TAP > undefined 20`] = `
+--- actual
++++ expected
+ Object {
+   "s": Set {
++    1,
+     2,
+     3,
+     4,
+-    5,
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 21`] = `
+--- actual
++++ expected
+ Object {
+   "cs": Set {
+     Object {
+       "a": 1,
+     },
+     Object {
+       "a": 1,
+     },
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 22`] = `
+--- actual
++++ expected
+ Object {
+   "cs": Set {
+     Object {
+       "a": 1,
+     },
+     Object {
+       "a": 1,
+     },
+-    Object {
+-      "a": 1,
+-    },
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 23`] = `
+--- actual
++++ expected
+ Object {
+   "b": Buffer <61 73 64 66>,
+ }
+`
+
+exports[`test/has.js TAP > undefined 24`] = `
+--- actual
++++ expected
+ Object {
+-  "b": Buffer <61 73 64>,
++  "b": Buffer <61 73 64 66>,
+ }
+`
+
+exports[`test/has.js TAP > undefined 25`] = `
+--- actual
++++ expected
+ Object {
+   "d": 2019-02-14T07:41:12.747Z,
+ }
+`
+
+exports[`test/has.js TAP > undefined 26`] = `
+--- actual
++++ expected
+ Object {
+-  "d": 1979-07-01T00:00:00.000Z,
++  "d": 2019-02-14T07:41:12.747Z,
+ }
+`
+
+exports[`test/has.js TAP > undefined 27`] = `
+--- actual
++++ expected
+ Object {
+   "a": Array [
+     1,
+     2,
+     3,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 28`] = `
+--- actual
++++ expected
+ Object {
+   "a": Array [
+     1,
+     2,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 29`] = `
+--- actual
++++ expected
+ Object {
+   "a": Array [],
+ }
+`
+
+exports[`test/has.js TAP > undefined 30`] = `
+--- actual
++++ expected
+ Object {
+   "ea": Array [],
+ }
+`
+
+exports[`test/has.js TAP > undefined 31`] = `
+--- actual
++++ expected
+ Object {
+   "ea": Array [
+-    1,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 32`] = `
+--- actual
++++ expected
+ Object {
+   "a": Array [
+     1,
+     2,
+     3,
+-    4,
+   ],
+ }
+`
+
+exports[`test/has.js TAP > undefined 33`] = `
+--- actual
++++ expected
+ Object {
+   "c": Object {},
+ }
+`
+
+exports[`test/has.js TAP > undefined 34`] = `
+--- actual
++++ expected
+ Object {
+   "c": Object {
+     "s": Set {},
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 35`] = `
+--- actual
++++ expected
+ Object {
+   "c": Object {
+     "s": Set {
+       Map {},
+     },
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 36`] = `
+--- actual
++++ expected
+ Object {
+   "c": Object {
+     "s": Set {
+       Map {
+         undefined => undefined,
+       },
+     },
+   },
+ }
+`
+
+exports[`test/has.js TAP > undefined 37`] = `
+--- actual
++++ expected
+ Object {
+-  "xyz": true,
+ }
+`
+
+exports[`test/has.js TAP > undefined 38`] = `
+--- actual
++++ expected
+ Object {
+   "c": Object {
+     "s": Set {
+       Map {
+         undefined => undefined,
+       },
+     },
+   },
+ }
+`
---
+++ test/has.js
@@ -0,0 +1,69 @@
+const t = require('tap')
+const compare = require('../')
+const has = (t, a, b) => {
+  const h = compare.has(a, b)
+  t.matchSnapshot(h.diff)
+  return h.match
+}
+
+const arr = [1, 2, 3]
+const a = {
+  m: new Map([[1,2],[3,4]]),
+  cm: new Map([[{a: 1}, 1], [{a: 1}, 1], [{b:2}, 2], [{b:2}, 2]]),
+  em: new Map(),
+  s: new Set([1, 2, 3, 4]),
+  cs: new Set([{a:1},{a:1},{b:2},{b:2}]),
+  es: new Set(),
+  b: Buffer.from('asdf'),
+  d: new Date('2019-02-14T07:41:12.747Z'),
+  a: [1,2,3],
+  ea: [],
+  i: {[Symbol.iterator]: function*() { for (let i of arr) { yield i }}},
+  c: { s: new Set() },
+}
+const m = new Map([a.c, a])
+a.c.s.add(m)
+
+t.ok(has(t, a, { i: [1, 2, 3] }))
+t.ok(has(t, a, { i: (function(){return arguments})(1, 2, 3) }))
+t.ok(has(t, a, { i: (function(){return arguments})(1, 2, 3) }))
+t.ok(has(t, a, { a: (function(){return arguments})(1, 2) }))
+t.ok(has(t, a, { i: (function(){return arguments})(1, 2) }))
+t.ok(has(t, a, { m: new Map([[1, 2]]) }))
+t.ok(has(t, a, { m: new Map([[3, 4]]) }))
+t.ok(has(t, a, { m: new Map([['3', 4]]) }))
+t.ok(has(t, a, { cm: new Map([[{a: 1}, 1], [{a: 1}, 1]])}))
+t.notOk(has(t, a, { m: new Map([[3, 4], ['3', 4]]) }))
+t.ok(has(t, a, { m: new Map([[3, 4], [1, 2]]) }))
+t.ok(has(t, a, { m: new Map([[1, 2], [3, 4]]) }))
+t.notOk(has(t, a, { m: new Map([[1, 2], [4, 3]]) }))
+t.notOk(has(t, a, { em: new Map([[1, 2]]) }))
+t.notOk(has(t, a, { em: new Map([[1, 2]]) }))
+t.ok(has(t, a, { em: new Map() }))
+t.ok(has(t, a, { s: new Set([3, 2]) }))
+t.ok(has(t, a, { es: new Set() }))
+t.ok(has(t, a, { s: new Set() }))
+t.notOk(has(t, a, { s: new Set([2, 3, 4, 5]) }))
+t.ok(has(t, a, {cs: new Set([{a:1},{a:1}])}))
+t.notOk(has(t, a, {cs: new Set([{a:1},{a:1},{a:1}])}))
+t.ok(has(t, a, { b: Buffer.from('asdf') }))
+t.notOk(has(t, a, { b: Buffer.from('asd') }))
+t.ok(has(t, a, { d: new Date(a.d.toISOString()) }))
+t.notOk(has(t, a, { d: new Date('1979-07-01') }))
+t.ok(has(t, a, { a: [1,2,3] }))
+t.ok(has(t, a, { a: [1,2] }))
+t.ok(has(t, a, { a: [] }))
+t.ok(has(t, a, { ea: [] }))
+t.notOk(has(t, a, { ea: [1] }))
+t.notOk(has(t, a, { a: [1,2,3,4] }))
+t.ok(has(t, a, {c: {}}))
+t.ok(has(t, a, {c: {s: new Set()}}))
+t.ok(has(t, a, {c: {s: new Set([new Map()])}}))
+t.ok(has(t, a, {c: {s: new Set([new Map([{s: new Set()}, a])])}}))
+t.notOk(has(t, a, {xyz: true}))
+
+// same circularity
+const b = { c: { s: new Set() } }
+const n = new Map([b.c, b])
+b.c.s.add(n)
+t.ok(has(t, a, b))
