3f3ceb0e4450f3c4f9562046ca1c54dac490f19b
isaacs <i@izs.me>
2019-02-13 22:59:53 -0800

    tests for Strict

---
+++ tap-snapshots/test-strict.js-TAP.test.js
@@ -0,0 +1,907 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/strict.js TAP symbology > undefined 1`] = `
+--- actual
++++ expected
+ Object {
+   "a": Symbol(a),
+ }
+`
+
+exports[`test/strict.js TAP symbology > undefined 2`] = `
+--- actual
++++ expected
+ Symbol(a)
+`
+
+exports[`test/strict.js TAP symbology > undefined 3`] = `
+--- actual
++++ expected
+ Object {
+-  "a": Symbol(a),
++  "a": Symbol(a),
+ }
+`
+
+exports[`test/strict.js TAP symbology > undefined 4`] = `
+--- actual
++++ expected
+ Object {
+-  "a": Symbol(a),
++  "a": Symbol(a),
+ }
+`
+
+exports[`test/strict.js TAP symbology > undefined 5`] = `
+--- actual
++++ expected
+ Object {
+-  "a": [Function Symbol],
++  "a": Symbol(a),
+ }
+`
+
+exports[`test/strict.js TAP symbology > undefined 6`] = `
+--- actual
++++ expected
+ Object {
+-  "a": "Symbol(a)",
++  "a": Symbol(a),
+ }
+`
+
+exports[`test/strict.js TAP symbology > undefined 7`] = `
+--- actual
++++ expected
+ Object {
+-  "a": Symbol(a),
++  "a": "Symbol(a)",
+ }
+`
+
+exports[`test/strict.js TAP symbology > undefined 8`] = `
+--- actual
++++ expected
+ Object {
+-  "a": Symbol(a),
++  "a": "Symbol(a)",
+ }
+`
+
+exports[`test/strict.js TAP symbology > undefined 9`] = `
+--- actual
++++ expected
+ Object {
+-  "a": [Function Symbol],
++  "a": "Symbol(a)",
+ }
+`
+
+exports[`test/strict.js TAP array-likes > undefined 1`] = `
+--- actual
++++ expected
+ Array [
+   1,
+   2,
+   3,
+ ]
+`
+
+exports[`test/strict.js TAP array-likes > undefined 2`] = `
+--- actual
++++ expected
+-Array [
+-  1,
+-  2,
+-  3,
+-]
++Array [
++  1,
++  2,
++  3,
++]
+
+`
+
+exports[`test/strict.js TAP basic > undefined 1`] = `
+--- actual
++++ expected
+ null
+`
+
+exports[`test/strict.js TAP basic > undefined 2`] = `
+--- actual
++++ expected
+ undefined
+`
+
+exports[`test/strict.js TAP basic > undefined 3`] = `
+--- actual
++++ expected
+ 0
+`
+
+exports[`test/strict.js TAP basic > undefined 4`] = `
+--- actual
++++ expected
+ null
+`
+
+exports[`test/strict.js TAP basic > undefined 5`] = `
+--- actual
++++ expected
+ "ok"
+`
+
+exports[`test/strict.js TAP basic > undefined 6`] = `
+--- actual
++++ expected
+ [Function functionA]
+`
+
+exports[`test/strict.js TAP basic > undefined 7`] = `
+--- actual
++++ expected
+ Buffer <61 62 63>
+`
+
+exports[`test/strict.js TAP basic > undefined 8`] = `
+--- actual
++++ expected
+ 2001-01-11T00:00:00.000Z
+`
+
+exports[`test/strict.js TAP basic > undefined 9`] = `
+--- actual
++++ expected
+ /^h[oe][wl][dl][oy]$/
+`
+
+exports[`test/strict.js TAP basic > undefined 10`] = `
+--- actual
++++ expected
+Object {}
+`
+
+exports[`test/strict.js TAP basic > undefined 11`] = `
+--- actual
++++ expected
+ Object {
+   "a": "a",
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 12`] = `
+--- actual
++++ expected
+ Object {
+   "a": "a",
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 13`] = `
+--- actual
++++ expected
+ Array [
+   0,
+   1,
+ ]
+`
+
+exports[`test/strict.js TAP basic > undefined 14`] = `
+--- actual
++++ expected
+ EventEmitter {
+   "domain": Domain {
+     "domain": null,
+     "_events": Object {
+       "removeListener": [Function updateExceptionCapture],
+       "newListener": [Function updateExceptionCapture],
+       "error": [Function],
+     },
+     "_eventsCount": 3,
+     "_maxListeners": undefined,
+     "members": Array [],
+   },
+   "_events": Object {
+     "error": [Function onerror],
+   },
+   "_eventsCount": 1,
+   "_maxListeners": undefined,
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 15`] = `
+--- actual
++++ expected
+ &ref_1 Object {
+   "x": <*ref_1>,
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 16`] = `
+--- actual
++++ expected
+ &ref_1 Object {
+   "v": Object {
+     "v": Object {
+       "v": Object {
+         "v": Object {
+           "v": Object {
+             "v": Object {
+               "v": Object {
+                 "v": Object {
+                   "v": Object {
+                     "v": Object {
+                       "v": Object {
+                         "v": Object {
+                           "v": Object {
+                             "v": Object {
+                               "v": Object {
+                                 "v": <*ref_1>,
+                               },
+                             },
+                           },
+                         },
+                       },
+                     },
+                   },
+                 },
+               },
+             },
+           },
+         },
+       },
+     },
+   },
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 17`] = `
+--- actual
++++ expected
+ &ref_1 Object {
+   "nothin": null,
+   "nope": undefined,
+   "number": 0,
+   "funky": [Function functionA],
+   "stringer": "heya",
+   "then": 1981-03-30T00:00:00.000Z,
+   "rexpy": /^(pi|π)$/,
+   "granular": Object {
+     "stuff": Array [
+       0,
+       1,
+       2,
+     ],
+     "self": <*ref_1>,
+   },
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 18`] = `
+--- actual
++++ expected
+ &ref_1 Object {
+   "nothin": null,
+   "nope": undefined,
+   "number": 0,
+   "funky": [Function functionA],
+   "stringer": "heya",
+   "then": 1981-03-30T00:00:00.000Z,
+   "rexpy": /^(pi|π)$/,
+   "granular": Object {
+     "stuff": Array [
+       0,
+       1,
+       2,
+     ],
+     "self": Object {
+       "nothin": null,
+       "nope": undefined,
+       "number": 0,
+       "funky": [Function functionA],
+       "stringer": "heya",
+       "then": 1981-03-30T00:00:00.000Z,
+       "rexpy": /^(pi|π)$/,
+       "granular": Object {
+         "stuff": Array [
+           0,
+           1,
+           2,
+         ],
+         "self": <*ref_1>,
+       },
+     },
+   },
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 19`] = `
+--- actual
++++ expected
+-null
++null
+
+`
+
+exports[`test/strict.js TAP basic > undefined 20`] = `
+--- actual
++++ expected
+-"1"
++1
+
+`
+
+exports[`test/strict.js TAP basic > undefined 21`] = `
+--- actual
++++ expected
+-"nok"
++"ok"
+
+`
+
+exports[`test/strict.js TAP basic > undefined 22`] = `
+--- actual
++++ expected
+-"0"
++0
+
+`
+
+exports[`test/strict.js TAP basic > undefined 23`] = `
+--- actual
++++ expected
+-null
++undefined
+
+`
+
+exports[`test/strict.js TAP basic > undefined 24`] = `
+--- actual
++++ expected
+-[Function nop]
++[Function nop]
+
+`
+
+exports[`test/strict.js TAP basic > undefined 25`] = `
+--- actual
++++ expected
+-Object {}
++undefined
+
+`
+
+exports[`test/strict.js TAP basic > undefined 26`] = `
+--- actual
++++ expected
+-null
++Object {}
+
+`
+
+exports[`test/strict.js TAP basic > undefined 27`] = `
+--- actual
++++ expected
+-Object {}
++null
+
+`
+
+exports[`test/strict.js TAP basic > undefined 28`] = `
+--- actual
++++ expected
+-undefined
++null
+
+`
+
+exports[`test/strict.js TAP basic > undefined 29`] = `
+--- actual
++++ expected
+-null
++undefined
+
+`
+
+exports[`test/strict.js TAP basic > undefined 30`] = `
+--- actual
++++ expected
+-Buffer <61 62 63 64>
++Buffer <61 62 63>
+
+`
+
+exports[`test/strict.js TAP basic > undefined 31`] = `
+--- actual
++++ expected
+-Buffer <61 62 64>
++Buffer <61 62 63>
+
+`
+
+exports[`test/strict.js TAP basic > undefined 32`] = `
+--- actual
++++ expected
+-2001-01-12T00:00:00.000Z
++2001-01-11T00:00:00.000Z
+
+`
+
+exports[`test/strict.js TAP basic > undefined 33`] = `
+--- actual
++++ expected
+-/^(howdy|hello)$/
++/^h[oe][wl][dl][oy]$/
+
+`
+
+exports[`test/strict.js TAP basic > undefined 34`] = `
+--- actual
++++ expected
+ Arguments [
+   1,
+   2,
+   3,
+ ]
+`
+
+exports[`test/strict.js TAP basic > undefined 35`] = `
+--- actual
++++ expected
+ Arguments [
+   1,
+   2,
+   3,
+ ]
+`
+
+exports[`test/strict.js TAP basic > undefined 36`] = `
+--- actual
++++ expected
+-Array [
+-  1,
+-  2,
+-  3,
+-]
++Arguments [
++  1,
++  2,
++  3,
++]
+
+`
+
+exports[`test/strict.js TAP basic > undefined 37`] = `
+--- actual
++++ expected
+-Arguments [
+-  1,
+-  2,
+-  3,
+-]
++Array [
++  1,
++  2,
++  3,
++]
+
+`
+
+exports[`test/strict.js TAP basic > undefined 38`] = `
+--- actual
++++ expected
+-Object {}
++Array []
+
+`
+
+exports[`test/strict.js TAP basic > undefined 39`] = `
+--- actual
++++ expected
+-Object {}
++Object {}
+
+`
+
+exports[`test/strict.js TAP basic > undefined 40`] = `
+--- actual
++++ expected
+ Object {
++  "a": "a",
+-  "b": "b",
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 41`] = `
+--- actual
++++ expected
+ Object {
+   "b": "b",
+-  "c": undefined,
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 42`] = `
+--- actual
++++ expected
+ EventEmitter {
+   "domain": Domain {
+     "domain": null,
+     "_events": Object {
+       "removeListener": [Function updateExceptionCapture],
+       "newListener": [Function updateExceptionCapture],
+       "error": [Function],
+     },
+     "_eventsCount": 3,
+     "_maxListeners": undefined,
+     "members": Array [],
+   },
+   "_events": Object {
+     "error": [Function onerror],
+-    "data": [Function ondata],
+   },
+-  "_eventsCount": 2,
++  "_eventsCount": 1,
+   "_maxListeners": undefined,
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 43`] = `
+--- actual
++++ expected
+ &ref_1 Object {
+   "nothin": null,
+   "nope": undefined,
+   "number": 0,
+   "funky": [Function functionA],
+   "stringer": "heya",
+   "then": 1981-03-30T00:00:00.000Z,
+   "rexpy": /^(pi|π)$/,
+   "granular": Object {
+     "stuff": Array [
+       0,
+       1,
+-      3,
++      2,
+     ],
+     "self": Object {
+       "nothin": null,
+       "nope": undefined,
+       "number": 0,
+       "funky": [Function functionA],
+       "stringer": "heya",
+       "then": 1981-03-30T00:00:00.000Z,
+       "rexpy": /^(pi|π)$/,
+       "granular": Object {
+         "stuff": Array [
+           0,
+           1,
+-          2,
++          3,
+         ],
+         "self": <*ref_1>,
+       },
+     },
+   },
+ }
+`
+
+exports[`test/strict.js TAP basic > undefined 44`] = `
+--- actual
++++ expected
+ &ref_1 Object {
+   "nothin": null,
+   "nope": undefined,
+   "number": 0,
+   "funky": [Function functionA],
+   "stringer": "heya",
+   "then": 1981-03-30T00:00:00.000Z,
+   "rexpy": /^(pi|π)$/,
+   "granular": Object {
+     "stuff": Array [
+       0,
+       1,
+       2,
+     ],
+     "self": Object {
+       "nothin": null,
+       "nope": undefined,
+       "number": 0,
+       "funky": [Function functionA],
+       "stringer": "heya",
+       "then": 1981-03-30T00:00:00.000Z,
+       "rexpy": /^(pi|π)$/,
+       "granular": Object {
+         "stuff": Array [
+           0,
+           1,
+           2,
+         ],
+         "self": <*ref_1>,
+       },
+     },
+   },
+ }
+`
+
+exports[`test/strict.js TAP NaN > undefined 1`] = `
+--- actual
++++ expected
+ null
+`
+
+exports[`test/strict.js TAP set > undefined 1`] = `
+--- actual
++++ expected
+ Set {
+   1,
+   2,
+   3,
+   4,
+   Object {
+     "a": 1,
+   },
+ }
+`
+
+exports[`test/strict.js TAP set > undefined 2`] = `
+--- actual
++++ expected
+ Set {
+   1,
+   2,
+   3,
+   4,
+   Object {
+     "a": 1,
+   },
+ }
+`
+
+exports[`test/strict.js TAP set > undefined 3`] = `
+--- actual
++++ expected
+ Set {
+   Object {
+     "a": 1,
+   },
+   2,
+   4,
+   3,
+   1,
+ }
+`
+
+exports[`test/strict.js TAP set > undefined 4`] = `
+--- actual
++++ expected
+ Set {
+   1,
+-  2,
+ }
+`
+
+exports[`test/strict.js TAP set > undefined 5`] = `
+--- actual
++++ expected
+ Set {
+   1,
++  3,
++  5,
+-  6,
+-  2,
+ }
+`
+
+exports[`test/strict.js TAP set > undefined 6`] = `
+--- actual
++++ expected
+Set {}
+`
+
+exports[`test/strict.js TAP set > undefined 7`] = `
+--- actual
++++ expected
+-Array [
+-  1,
+-  2,
+-  3,
+-  4,
+-  Object {
+-    "a": 1,
+-  },
+-]
++Set {
++  1,
++  2,
++  3,
++  4,
++  Object {
++    "a": 1,
++  },
++}
+
+`
+
+exports[`test/strict.js TAP map > undefined 1`] = `
+--- actual
++++ expected
+ Map {
+   1 => 2,
+   3 => 4,
+   5 => Object {
+     "a": 1,
+   },
+   Object {
+     "a": 1,
+   } => 6,
+ }
+`
+
+exports[`test/strict.js TAP map > undefined 2`] = `
+--- actual
++++ expected
+ Map {
+   1 => 2,
+   3 => 4,
+   5 => Object {
+-    "a": "1",
++    "a": 1,
+   },
+   Object {
+     "a": 1,
+   } => 6,
+ }
+`
+
+exports[`test/strict.js TAP map > undefined 3`] = `
+--- actual
++++ expected
+ Map {
+   3 => 4,
+   5 => Object {
+-    "a": "1",
++    "a": 1,
+   },
+   Object {
+     "a": 1,
+   } => 6,
+   1 => 2,
+ }
+`
+
+exports[`test/strict.js TAP map > undefined 4`] = `
+--- actual
++++ expected
+ Map {
+   3 => 4,
+   5 => Object {
+-    "a": "1",
++    "a": 1,
+   },
+   Object {
+     "a": 1,
+   } => 6,
+   1 => 2,
+ }
+`
+
+exports[`test/strict.js TAP map > undefined 5`] = `
+--- actual
++++ expected
+ Map {
+   1 => 2,
+   3 => 4,
+   5 => Object {
+     "a": 1,
+   },
+   Object {
+     "a": 1,
+   } => 6,
+ }
+`
+
+exports[`test/strict.js TAP map > undefined 6`] = `
+--- actual
++++ expected
+ Map {
+   3 => 4,
+   5 => Object {
+     "a": 1,
+   },
+   Object {
+     "a": 1,
+   } => 6,
+   1 => 2,
+ }
+`
+
+exports[`test/strict.js TAP map > undefined 7`] = `
+--- actual
++++ expected
+Map {}
+`
+
+exports[`test/strict.js TAP map > undefined 8`] = `
+--- actual
++++ expected
+-Array [
+-  Array [
+-    1,
+-    2,
+-  ],
+-  Array [
+-    3,
+-    4,
+-  ],
+-  Array [
+-    5,
+-    Object {
+-      "a": 1,
+-    },
+-  ],
+-  Array [
+-    Object {
+-      "a": 1,
+-    },
+-    6,
+-  ],
+-]
++Map {
++  1 => 2,
++  3 => 4,
++  5 => Object {
++    "a": 1,
++  },
++  Object {
++    "a": 1,
++  } => 6,
++}
+
+`
+
+exports[`test/strict.js TAP map > undefined 9`] = `
+--- actual
++++ expected
+ Map {
+   1 => 2,
+   3 => 4,
+   5 => Object {
+     "a": 1,
+   },
++  Object {
++    "a": 1,
++  } => 6,
+-  Object {
+-    "a": 1,
+-    "b": 2,
+-  } => 6,
+ }
+`
+
+exports[`test/strict.js TAP map > undefined 10`] = `
+--- actual
++++ expected
+ Map {
+   3 => 4,
+   5 => Object {
+-    "a": 1,
++    "a": "1",
+   },
++  Object {
++    "a": 1,
++  } => 6,
+   1 => 2,
+-  Object {
+-    "a": 1,
+-    "b": 2,
+-  } => 6,
+ }
+`
