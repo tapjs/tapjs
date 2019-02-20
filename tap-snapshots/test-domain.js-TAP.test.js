2f18989b5d866b31f02d7a393e987a249ceebb6b
isaacs <i@izs.me>
2019-02-19 17:21:35 -0800

    add handling for domains
    They need to be treated specially in tap, because otherwise they result
    in gigantic YAML dumps with a ton of not very useful data.

---
+++ tap-snapshots/test-domain.js-TAP.test.js
@@ -0,0 +1,23 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/domain.js TAP > first stringify 1`] = `
+!domain {}
+
+`
+
+exports[`test/domain.js TAP > parsed stringified 1`] = `
+Domain {
+  domain: null,
+  _events:
+   [Object: null prototype] {
+     removeListener: [Function: updateExceptionCapture],
+     newListener: [Function: updateExceptionCapture] },
+  _eventsCount: 2,
+  _maxListeners: undefined,
+  members: [] }
+`
