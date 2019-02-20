28d1adf900938687c4b70f6df5b04ec35733b03e
isaacs <i@izs.me>
2019-02-20 14:08:08 -0800

    run all units on 'npm run unit' without arg

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 if [ "${1}" = "" ]; then
-  echo "Must provide unit name" >&2
-  exit 1
+  units=($(cd test &>/dev/null; ls))
+  for u in "${units[@]}"; do
+    u=$(basename "$u" .js)
+    if ! [ $u == "clean-stacks" ]; then
+      $BASH scripts/unit.sh "$u" || exit 1
+    fi
+  done
+else
+  shopt -s extglob
+  include=$(echo +(bin|lib)/$1.js)
+  if [ -f "$include" ]; then
+    set -x
+    node bin/run.js --100 test/${1}* --nyc-arg=--include="$include" -J
+  else
+    set -x
+    node bin/run.js test/${1}*
+  fi
 fi
-set -x
-node bin/run.js --100 test/${1}* --nyc-arg=--include={bin,lib}/$1.js -J
