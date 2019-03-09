ffe2eaa087efd9bafa4bcfe473c487de1cbac752
isaacs <i@izs.me>
2019-03-08 23:17:25 -0800

    test for --test-regexp defaults

---
+++ test/run/test-regex.js
@@ -0,0 +1,33 @@
+const {
+  tmpfile,
+  clean,
+  bin,
+  tap,
+  node,
+  dir,
+  t,
+  run,
+} = require('./')
+
+t.test('no args, pull in default files, not exclusions', t => {
+  tmpfile(t, 'file.spec.js', `
+    const t = require(${tap})
+    t.pass('this is fine')
+  `)
+  tmpfile(t, 'node_modules/bad.test.js', `
+    const t = require(${tap})
+    t.fail('should not run this')
+  `)
+  run([], { cwd: dir, env: { _TAP_IS_TTY: '1' }}, (er, o, e) => {
+    t.equal(er, null)
+    t.matchSnapshot(clean(o), 'output')
+    t.end()
+  })
+})
+
+t.test('error out if loading files fails', t => {
+  run([], { cwd: '/dev', env: { _TAP_IS_TTY: '1' }}, (er, o, e) => {
+    t.match(er, { code: 1 })
+    t.end()
+  })
+})
