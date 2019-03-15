e9d4c819af518fb3c783de1ce2dcfbff321286db
isaacs <i@izs.me>
2019-03-15 15:09:56 -0700

    split the run/languages.js test into multiple suites
    They run really slow, because child processes and transpiling

---
+++ test/run/ts.js
@@ -0,0 +1,33 @@
+const {
+  tmpfile,
+  run,
+  clean,
+  tap,
+  t,
+} = require('./')
+
+t.test('ts', t => {
+  const ok = tmpfile(t, 'ts/ok.ts', `
+    import * as t from ${tap}
+    t.pass('this is fine')
+  `)
+  run([ok], {}, (er, o, e) => {
+    t.equal(er, null)
+    t.matchSnapshot(clean(o))
+    t.end()
+  })
+})
+
+t.test('tsx', t => {
+  const ok = tmpfile(t, 'tsx/ok.tsx', `
+    import * as React from 'react'
+    import * as t from ${tap}
+    const div = (<div>Hello</div>)
+    t.pass('this is fine')
+  `)
+  run([ok], {}, (er, o, e) => {
+    t.equal(er, null)
+    t.matchSnapshot(clean(o))
+    t.end()
+  })
+})
