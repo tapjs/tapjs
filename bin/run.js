4f2ff5026954ab932279d4711c3341a3da746137
isaacs <i@izs.me>
2019-04-24 10:49:40 -0700

    fix typo in nyc --extension flags

--- bin/run.js
+++ bin/run.js
@@ -329,11 +329,11 @@ const runNyc = (cmd, programArgs, options, spawnOpts) => {
     '--lines=' + options.lines,
     '--statements=' + options.statements,
     '--reporter=' + reporter,
-    '--extensions=.js',
-    '--extensions=.jsx',
-    '--extensions=.mjs',
-    '--extensions=.ts',
-    '--extensions=.tsx',
+    '--extension=.js',
+    '--extension=.jsx',
+    '--extension=.mjs',
+    '--extension=.ts',
+    '--extension=.tsx',
     ...(options['nyc-arg'] || []),
   ]
   if (options['check-coverage'])
