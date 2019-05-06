ecfece69172f7ac2ede74faeb2d91070391478ae
isaacs <i@izs.me>
2019-05-06 14:52:40 -0700

    Add --jsx and --ts flags
    This allows users to do `tap --no-ts --node-arg=--require=my-ts-loader`
    to specify their own TypeScript loader that might conflict with tap's
    built-in version.
    
    Fix #550
    Fix #549

--- test/run/ts.js
+++ test/run/ts.js
@@ -30,3 +30,19 @@ t.test('tsx', t => {
     t.end()
   })
 })
+
+t.test('ts manually', t => {
+  const ok = tmpfile(t, 'mixed/ok.js', `
+    require('./foo.ts')
+  `)
+  const foots = tmpfile(t, 'mixed/foo.ts', `
+    import * as t from ${tap}
+    t.pass('this is fine')
+  `)
+  const args = [ok, foots, '--no-ts', '--node-arg=--require=ts-node/register']
+  run(args, {}, (er, o, e) => {
+    t.equal(er, null)
+    t.matchSnapshot(o)
+    t.end()
+  })
+})
