36ddfd94a102eda917f42615599f005cdab7c25c
isaacs <i@izs.me>
2021-02-16 12:20:25 -0800

    minor style nits, and remove call to removed t.done() alias

--- test/test-esm.js
+++ test/test-esm.js
@@ -3,9 +3,9 @@ let t;
 try {
   t = require('tap')
 } catch (e) {
-  t = require('../');
+  t = require('../')
   t.equals(e.code, 'MODULE_NOT_FOUND')
-  t.end();
+  t.end()
   return
 }
 
@@ -14,6 +14,6 @@ const {join} = require('path')
 const testPath = join(__dirname, './test.mjs')
 
 t.test('test esm entry point', t => {
-  t.spawn('node', [testPath]);
-  t.done();
+  t.spawn('node', [testPath])
+  t.end()
 })
