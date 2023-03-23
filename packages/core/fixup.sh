5b8c5468425a7a0af952bd6b2383e428fdb8d16a
isaacs <i@izs.me>
2023-03-23 11:56:59 -0700

    help tsc be comfortable with the cyclical package dep

--- packages/core/fixup.sh
+++ packages/core/fixup.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+rm -rf dist
+mv dist-tmp dist
+
+# the tap-dir module has to look at either __dirname or
+# import.meta.url, and each blows up in the other import
+# system.
+mv dist/cjs/tap-dir-cjs.js dist/cjs/tap-dir.js
+rm dist/cjs/tap-dir.js.map
+mv dist/cjs/tap-dir-cjs.d.ts dist/cjs/tap-dir.d.ts
+rm dist/cjs/tap-dir.d.ts.map
+
 cat >dist/cjs/package.json <<!EOF
 {
   "type": "commonjs"
@@ -11,8 +22,3 @@ cat >dist/mjs/package.json <<!EOF
   "type": "module"
 }
 !EOF
-
-mv dist/cjs/tap-dir-cjs.js dist/cjs/tap-dir.js
-rm dist/cjs/tap-dir.js.map
-mv dist/cjs/tap-dir-cjs.d.ts dist/cjs/tap-dir.d.ts
-rm dist/cjs/tap-dir.d.ts.map
