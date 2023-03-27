51b6271ebf02766493f63b0a440065b75bd7fd25
isaacs <i@izs.me>
2023-03-27 10:34:38 -0700

    core/stack: get call site from string capture
    The direct CallSite capture has some issues with Proxy objects, it seems.

--- src/core/src/stack.ts
+++ src/core/src/stack.ts
@@ -1,6 +1,31 @@
-import StackUtils from 'stack-utils'
+import StackUtils, { CallSiteLike } from 'stack-utils'
+import { tapDir } from './tap-dir.js'
 
-export default new StackUtils({
-  internals: StackUtils.nodeInternals(),
-  ignoredPackages: []
+const regExpEscape = (s: string) =>
+  s.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+
+const stack = new StackUtils({
+  internals: [
+    ...StackUtils.nodeInternals(),
+    new RegExp(regExpEscape(tapDir)),
+  ],
+  ignoredPackages: [
+    '@tapjs/core',
+    'ts-node',
+    '@tapjs/.test-built',
+    'function-loop',
+  ],
 })
+
+const origStackAt = stack.at
+stack.at = (fn: (...a: any[]) => any): CallSiteLike => {
+  const st = stack.captureString(5, fn).split(/\n/)
+  for (const line of st) {
+    const p = stack.parseLine(line)
+    if (!p) continue
+    return p
+  }
+  return origStackAt.call(stack, fn)
+}
+
+export default stack
