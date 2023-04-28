39831870b0b35ed2fcb94a9e3f0f9a446afd3ad4
isaacs <i@izs.me>
2023-04-28 15:08:03 -0700

    update scratch test

--- src/core/test/scratch.ts
+++ src/core/test/scratch.ts
@@ -1,44 +1,11 @@
-import { Test } from '@tapjs/test'
+import { TAP } from '@tapjs/core'
 
-import { TestBaseOpts } from '@tapjs/core'
-const opts: TestBaseOpts = {
-  debug: /\btap\b/.test(process.env.NODE_DEBUG || ''),
-  name: 'TAP',
-}
+const t = TAP()
 
-const t = new Test(opts)
-
-t.stream.pipe(process.stdout)
-t.runMain(() => {
-  console.error('t.runMain callback')
-})
-
-t.beforeEach(() => {
-  console.log('first beforeEach')
-})
-t.beforeEach((t) => {
-  t.childId
-  t.afterEach
-  console.log('second beforeEach')
-})
-t.afterEach(() => {
-  console.log('parent aftereach')
-})
-
-t.test('hello', {}, t => {
-  t.pass('this is fine', { bail: true, diagnostic:true })
-  t.equal(1, 2, { skip: true })
-  t.beforeEach(() => {
-    console.log('child beforeeach')
+t.test('hello', async t => {
+  t.pass('spanning multiple lines', {
+    diagnostic: true,
   })
-  t.afterEach(() => {
-    console.log('child aftereach')
-  })
-  t.test('hello child', {}, t => {
-    t.pass('also fine')
-    t.end()
-  })
-  t.end()
+  t.pass('next line indented weirdly', { diagnostic: true })
+                  t.equal(2, 2, { todo: true })
 })
-
-t.end()
