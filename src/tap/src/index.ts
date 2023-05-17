dba12897436544579c96daa47c01367e69e32d06
isaacs <i@izs.me>
2023-05-16 22:59:40 -0700

    export bound tap methods as named exports

--- src/tap/src/index.ts
+++ src/tap/src/index.ts
@@ -10,3 +10,22 @@ export {
 } from '@tapjs/core'
 export { Test } from '@tapjs/test'
 export const t: TAP = tap()
+
+// People really like doing `import { test } from 'tap'`
+// this makes that work by binding these methods as named exports.
+//
+// We can't export anything defined in a plugin, because that might
+// not exist in the user's actual test environment, so the set is
+// pretty limited, but `test` and `plan` are the only ones normally
+// used in this way.
+export const plan = t.plan.bind(t)
+export const test = t.test.bind(t)
+export const skip = t.skip.bind(t)
+export const todo = t.todo.bind(t)
+export const bailout = t.bailout.bind(t)
+export const comment = t.comment.bind(t)
+export const timeout = t.timeout.bind(t)
+export const pragma = t.pragma.bind(t)
+export const pass = t.pass.bind(t)
+export const fail = t.fail.bind(t)
+export const end = t.end.bind(t)
