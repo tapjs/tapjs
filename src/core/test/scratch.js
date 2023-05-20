32ed07fee1231aca8310e4cabf0a0c472cd66253
isaacs <i@izs.me>
2023-05-19 21:57:09 -0700

    fix compiled scratch.js test file

--- src/core/test/scratch.js
+++ src/core/test/scratch.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@tapjs/core");
-const t = (0, core_1.TAP)();
+const t = (0, core_1.tap)();
 t.test('hello', async (t) => {
     t.pass('spanning multiple lines', {
         diagnostic: true,
