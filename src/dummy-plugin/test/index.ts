3c5d86c80b5dcaef631983043f6f866fab70ec60
isaacs <i@izs.me>
2023-06-17 22:13:19 -0700

    missing tests for dummy plugin

--- src/dummy-plugin/test/index.ts
+++ src/dummy-plugin/test/index.ts
@@ -7,8 +7,37 @@ t.test('loader has global preload only', async t => {
   t.matchOnly(loader, { globalPreload: Function })
   t.equal(loader.globalPreload(), 'global.DUMMY_PLUGIN_LOADED = true')
 })
-t.test('plugin adds methods', async t => {
+t.test('plugin adds stuff', async t => {
   t.equal(withPlug.testBase.name, 'TAP')
+  t.equal(withPlug.whoami(), withPlug)
+  const o = {}
+  t.equal(withPlug.whoami.call(o), o)
   t.equal(withPlug.method(), 1)
-  t.matchSnapshot(withPlug.dummyConfig)
+  t.strictSame(withPlug.dummyConfig, {
+    flag: false,
+    flagSet: [],
+    opt: '',
+    optSet: [],
+    num: 0,
+    numSet: [],
+  })
+  //@ts-expect-error
+  withPlug.dummyConfig = { foo: 'bar' }
+  t.strictSame(withPlug.dummyConfig, { foo: 'bar' })
+  //@ts-expect-error
+  withPlug.dummyConfig = null
+  process.env.TAP_DUMMY_FLAG_SET = '1\n0\n1'
+  process.env.TAP_DUMMY_OPT_SET = 'foo\nbar'
+  process.env.TAP_DUMMY_NUM_SET = '1\n2\n3'
+  process.env.TAP_DUMMY_FLAG = '1'
+  process.env.TAP_DUMMY_OPT = 'baz'
+  process.env.TAP_DUMMY_NUM = '123'
+  t.match(withPlug.dummyConfig, {
+    flag: true,
+    flagSet: [true, false, true],
+    opt: 'baz',
+    optSet: ['foo', 'bar'],
+    num: 123,
+    numSet: [1, 2, 3],
+  })
 })
