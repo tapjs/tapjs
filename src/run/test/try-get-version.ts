0c8459317d4c7017f50d283d6f856f3af40adbc8
isaacs <i@izs.me>
2023-07-14 18:59:51 -0700

    test tryGetVersion
    Also, make it WAYYYYY more 'best effort', since it was doing a ton of
    stuff that's really hard to test, and mostly unnecessary. If people
    want their version reported, they can export their package.json file,
    it's FINE.

---
+++ src/run/test/try-get-version.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+import t from 'tap'
+import { tryGetVersion } from '../dist/try-get-version.js'
+
+t.test('try and get the version', t => {
+  const dir = t.testdir({
+    pkg: {
+      'package.json': JSON.stringify({
+        no: {
+          version: 'here',
+        },
+      }),
+    },
+    nonpkg: {},
+  })
+
+  const expect = JSON.parse(
+    readFileSync(
+      new URL('../../tap/package.json', import.meta.url),
+      'utf8'
+    )
+  ).version
+
+  t.equal(tryGetVersion('tap'), expect)
+  t.equal(tryGetVersion(resolve(dir, 'pkg')), undefined)
+  t.equal(tryGetVersion(resolve(dir, 'nonpkg')), undefined)
+  t.end()
+})
