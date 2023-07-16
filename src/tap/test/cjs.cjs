f0bd3f80b58457ff8ea81cf6149860360c707cd4
isaacs <i@izs.me>
2023-07-15 23:44:55 -0700

    tap: remove extraneous exports line in cjs test

--- src/tap/test/cjs.cjs
+++ src/tap/test/cjs.cjs
@@ -3,4 +3,3 @@ const { default: DEF, ...extra } = require('../dist/cjs/cjs.cjs')
 const index = require('../dist/cjs/index.js')
 t.equal(DEF, t)
 t.strictSame({ ...extra }, { ...index })
-exports = {}
