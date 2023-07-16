df03954482bfe1984d9fafe2a607b52824efb849
isaacs <i@izs.me>
2023-07-15 23:43:55 -0700

    yaml: test coverage for !!timestamp
    Kind of unnecessary, since it's just a copy of the !!date tag test, but
    completeness is fun.

---
+++ src/yaml/tap-snapshots/test/types/timestamp.ts.test.cjs
@@ -0,0 +1,10 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/types/timestamp.ts > TAP > parsed stringified 1`] = `
+1979-07-01T00:00:00.000Z
+`
