7321bbd56750fc8fb6f9dc046061fad5375851bb
isaacs <i@izs.me>
2023-07-23 23:51:10 -0700

    correct some tsconfig errors in refactor

--- src/run/map.js
+++
@@ -1,3 +0,0 @@
-// some tests have to be .mjs or .cjs for reasons
-export default t =>
-  t.replace('test', 'src').replace(/\.[^.]+$/, '') + '.ts'
--- src/run/src/help.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.help = void 0;
-var help = function (_, config) {
-    return console.log(config.jack.usage());
-};
-exports.help = help;
