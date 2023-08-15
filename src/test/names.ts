0b264e2e06140e753eea96c596ccc7d5a74c716c
isaacs <i@izs.me>
2023-08-15 12:08:50 -0700

    remove some debugging code that snuck into last commit

--- src/test/names.ts
+++
@@ -1,9 +0,0 @@
-import t from 'tap'
-
-t.test('child test', t => {
-  for (const i in t) console.error(i)
-  console.error(Object.getOwnPropertyNames(t))
-  console.error(Object.keys(t))
-  console.error(String(t))
-  t.end()
-})
