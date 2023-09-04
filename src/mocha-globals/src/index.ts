0f2da045c4aaa39e9233bdcbb94084341ef27439
isaacs <i@izs.me>
2023-09-04 15:42:00 -0700

    tsdocs

--- src/mocha-globals/src/index.ts
+++ src/mocha-globals/src/index.ts
@@ -1,6 +1,6 @@
 import { TapPlugin, TestBase, TestBaseOpts } from '@tapjs/core'
-import { isPromise } from 'is-actual-promise'
 import { at } from '@tapjs/stack'
+import { isPromise } from 'is-actual-promise'
 
 import { plugin as AfterPlugin } from '@tapjs/after'
 import { plugin as AfterEachPlugin } from '@tapjs/after-each'
@@ -14,13 +14,32 @@ let currentTest: TestBase | undefined = undefined
 // the root where BDD/TDD interface is mounted
 let rootSuite: TestBase | undefined = undefined
 
+/**
+ * The function passed to it() block
+ *
+ * Calling with an error fails the test.
+ */
 export type Done = (err?: any) => void | any
+/**
+ * The method passed to describe()
+ */
 export type SuiteBlock = () => void | any
+/**
+ * The method passed to it()
+ */
 export type TestBlock =
   | (() => void | any | Promise<void | any>)
   | ((done: Done) => void | any | Promise<void | any>)
+/**
+ * The method passed to before(), after(), beforeEach(), or afterEach()
+ */
 export type HookBlock = () => void | any | Promise<void | any>
 
+/**
+ * Create a test suite
+ *
+ * Alias: context, suite
+ */
 export function describe(block: SuiteBlock): void
 export function describe(name: string, block?: SuiteBlock): void
 export function describe(
@@ -58,6 +77,11 @@ export function describe(
   })
 }
 
+/**
+ * Create a test within a suite.
+ *
+ * Alias: test
+ */
 export function it(): void
 export function it(name: string): void
 export function it(block: TestBlock): void
@@ -71,8 +95,8 @@ export function it(
       ? nameOrBlock
       : typeof nameOrBlock === 'function'
       ? nameOrBlock.name
-  /* c8 ignore start */
-      : undefined
+      : /* c8 ignore start */
+        undefined
   /* c8 ignore stop */
   if (typeof nameOrBlock === 'function') block = nameOrBlock
   const cs = getCurrentSuite()
@@ -88,7 +112,7 @@ export function it(
   }
 
   const opts: TestBaseOpts = {
-    at: at(it)
+    at: at(it),
   }
   if (!block) {
     opts.todo = true
@@ -169,6 +193,13 @@ const hookPre = (
   ]
 }
 
+/**
+ * Run a function at the start of the suite.
+ *
+ * Equivalent to t.before()
+ *
+ * Alias: suiteSetup()
+ */
 export function before(block: HookBlock): any | void
 export function before(name: string, block: HookBlock): any | void
 export function before(
@@ -188,6 +219,13 @@ export function before(
   suite.before(fn)
 }
 
+/**
+ * Run a function at the end of the suite.
+ *
+ * Equivalent to t.after() or t.teardown()
+ *
+ * Alias: suiteTeardown()
+ */
 export function after(block: HookBlock): any | void
 export function after(name: string, block: HookBlock): any | void
 export function after(
@@ -207,6 +245,14 @@ export function after(
   suite.after(fn)
 }
 
+/**
+ * Run a function before each test within the suite, and before
+ * all child suites and tests.
+ *
+ * Equivalent to t.beforeEach()
+ *
+ * Alias: setup()
+ */
 export function beforeEach(block: HookBlock): any | void
 export function beforeEach(name: string, block: HookBlock): any | void
 export function beforeEach(
@@ -231,6 +277,14 @@ export function beforeEach(
   suite.beforeEach(fn)
 }
 
+/**
+ * Run a function after each test within the suite, and after
+ * all child suites and tests.
+ *
+ * Equivalent to t.afterEach()
+ *
+ * Alias: teardown()
+ */
 export function afterEach(block: HookBlock): any | void
 export function afterEach(name: string, block: HookBlock): any | void
 export function afterEach(
@@ -255,13 +309,38 @@ export function afterEach(
   suite.afterEach(fn)
 }
 
+/**
+ * Get the current suite.
+ *
+ * Returns `undefined` if the interface is not mounted, or if not in
+ * a suite currently.
+ */
 const getCurrentSuite = () =>
   currentSuite && currentSuite.t !== rootSuite?.t
     ? currentSuite.t
     : undefined
+
+/**
+ * Get the current test.
+ *
+ * Returns `undefined` if the interface is not mounted, or if not in a test
+ * currently.
+ */
 const getCurrentTest = () => currentTest?.t
+
+/**
+ * Return the tap test that the interface is mounted on (by default, the root
+ * TAP object when the plugin is loaded) or `undefined` if not currently
+ * mounted.
+ */
 const getMounted = () => rootSuite?.t
 
+/**
+ * Returns a reference to the current test, current suite, or tap mount
+ * point. If not mounted, throws an error.
+ *
+ * This is useful for doing arbitrary tap stuff within the BDD/TDD interface.
+ */
 export const tt = (): Exclude<
   | ReturnType<typeof getMounted>
   | ReturnType<typeof getCurrentTest>
@@ -293,6 +372,13 @@ export {
   afterEach as teardown,
 }
 
+/**
+ * Mount the interface on a tap test.
+ *
+ * Throws an error if already mounted.
+ *
+ * Called by default on the root TAP object when the plugin is active.
+ */
 export const mount = (t: TestBase) => {
   if (rootSuite) {
     const er = new Error(
@@ -330,6 +416,12 @@ if (process.env.TAP_MOCHA_GLOBALS === '1') {
   globalize()
 }
 
+/**
+ * Plugin method used by tap.
+ *
+ * Doesn't return any extensions, just attaches to the first Test it
+ * encounters (by default, this is the root TAP test when active).
+ */
 export const plugin: TapPlugin<{}> = (t: TestBase) => {
   if (!rootSuite) {
     rootSuite = t
@@ -339,6 +431,12 @@ export const plugin: TapPlugin<{}> = (t: TestBase) => {
 }
 
 export const config = {
+  /**
+   * Automatically inject the BDD/TDD methods into the global environment.
+   *
+   * If this flag is disabled, then you may access these via by importing them
+   * from `@tapjs/mocha-globals` explicitly.
+   */
   'mocha-globals': {
     type: 'boolean',
     default: true,
