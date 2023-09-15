556e520e402712bf6c098e9f405b2fd2feeefa2b
isaacs <i@izs.me>
2023-09-14 20:50:11 -0700

    mock: actually be resilient against multiple instances
    Making the service key a global works only as long as there's
    exactly one instance of this module in the tree.
    
    Which, probably always going to be the case. But still. Better
    to actually not have that be an issue.

--- src/mock/src/service-key.ts
+++ src/mock/src/service-key.ts
@@ -1,17 +1,3 @@
-import { randomBytes } from 'crypto'
-
-// Need this to be the same for a given thread between both
-// CJS and ESM, since it might be loaded in the main thread via
-// the import.mjs (esm), the globalPreload script (cjs), and of course the
-// user's test program.
-const kServiceKey = Symbol.for('@tapjs/mock.globalServiceKey')
-const g = globalThis as typeof globalThis & {
-  [kServiceKey]?: string
-}
-
-// literally the only way to cover both branches would be to break
-// mocking entirely, so ignore this.
-export const serviceKey = (g[kServiceKey] =
-  /* c8 ignore start */
-  g[kServiceKey] ?? randomBytes(8).toString('hex'))
-/* c8 ignore stop */
+//@ts-ignore
+import serviceKeyCJS from '../commonjs/service-key.js'
+export const { serviceKey } = serviceKeyCJS as { serviceKey: string }
