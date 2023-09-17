e8760a08c94bb993da5e868d1b71deb4f68062e0
isaacs <i@izs.me>
2023-09-16 22:46:47 -0700

    chore: detect workspace changes in pj or scripts
    It's not just src that affects the output of the module. Changing
    build scripts can have a huge impact on the results.

--- scripts/changed-workspaces.sh
+++ scripts/changed-workspaces.sh
@@ -4,7 +4,7 @@ git show --name-only HEAD~$( \
   git describe \
   | xargs node -p 'process.argv[1].split("-").slice(-2).shift()'
 )..HEAD \
-  | grep -o -E 'src/[^/]+/src' \
+  | grep -o -E 'src/[^/]+/(src|package.json|scripts)' \
   | sort \
   | uniq \
   | awk -F/ '{ print $1 "/" $2 }'
