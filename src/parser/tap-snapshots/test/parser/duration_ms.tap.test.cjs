b03f951bbc3e47682a7cf9b97d86711072904ae3
isaacs <i@izs.me>
2023-09-18 15:32:17 -0700

    Treat duration_ms as a time directive
    Fix: https://github.com/tapjs/tapjs/issues/895

---
+++ src/parser/tap-snapshots/test/parser/duration_ms.tap.test.cjs
@@ -0,0 +1,432 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/parser.ts > TAP > duration_ms.tap > output bail=false 1`] = `
+Array [
+  Array [
+    "line",
+    "TAP version 14\\n",
+  ],
+  Array [
+    "version",
+    14,
+  ],
+  Array [
+    "line",
+    "# Subtest: subtest\\n",
+  ],
+  Array [
+    "child",
+    Array [
+      Array [
+        "comment",
+        "# Subtest: subtest\\n",
+      ],
+      Array [
+        "line",
+        "ok 1 - this is fine\\n",
+      ],
+      Array [
+        "assert",
+        Result {
+          "buffered": false,
+          "diag": null,
+          "fullname": "subtest > this is fine",
+          "id": 1,
+          "name": "this is fine",
+          "ok": true,
+          "plan": null,
+          "previous": null,
+          "skip": false,
+          "tapError": null,
+          "time": null,
+          "todo": false,
+        },
+      ],
+      Array [
+        "line",
+        "1..1\\n",
+      ],
+      Array [
+        "plan",
+        Plan {
+          "comment": "",
+          "end": 1,
+          "start": 1,
+        },
+      ],
+      Array [
+        "complete",
+        FinalResults {
+          "bailout": false,
+          "count": 1,
+          "fail": 0,
+          "failures": Array [],
+          "ok": true,
+          "pass": 1,
+          "passes": undefined,
+          "plan": FinalPlan {
+            "comment": "",
+            "end": 1,
+            "skipAll": false,
+            "skipReason": "",
+            "start": 1,
+          },
+          "skip": 0,
+          "skips": Array [],
+          "time": null,
+          "todo": 0,
+          "todos": Array [],
+        },
+      ],
+      Array [
+        "close",
+      ],
+    ],
+  ],
+  Array [
+    "line",
+    "    ok 1 - this is fine\\n",
+  ],
+  Array [
+    "result",
+    Result {
+      "buffered": false,
+      "diag": null,
+      "fullname": "subtest > this is fine",
+      "id": 1,
+      "name": "this is fine",
+      "ok": true,
+      "plan": null,
+      "previous": null,
+      "skip": false,
+      "tapError": null,
+      "time": null,
+      "todo": false,
+    },
+  ],
+  Array [
+    "pass",
+    Result {
+      "buffered": false,
+      "diag": null,
+      "fullname": "subtest > this is fine",
+      "id": 1,
+      "name": "this is fine",
+      "ok": true,
+      "plan": null,
+      "previous": null,
+      "skip": false,
+      "tapError": null,
+      "time": null,
+      "todo": false,
+    },
+  ],
+  Array [
+    "line",
+    "    1..1\\n",
+  ],
+  Array [
+    "line",
+    "ok 1 - subtest\\n",
+  ],
+  Array [
+    "line",
+    "  ---\\n",
+  ],
+  Array [
+    "line",
+    "  duration_ms: 1.432\\n",
+  ],
+  Array [
+    "line",
+    "  ...\\n",
+  ],
+  Array [
+    "assert",
+    Result {
+      "buffered": false,
+      "diag": Object {},
+      "fullname": "subtest",
+      "id": 1,
+      "name": "subtest",
+      "ok": true,
+      "plan": null,
+      "previous": null,
+      "skip": false,
+      "tapError": null,
+      "time": 1.432,
+      "todo": false,
+    },
+  ],
+  Array [
+    "line",
+    "1..1\\n",
+  ],
+  Array [
+    "plan",
+    Plan {
+      "comment": "",
+      "end": 1,
+      "start": 1,
+    },
+  ],
+  Array [
+    "line",
+    "# { total: 1, pass: 1 }\\n",
+  ],
+  Array [
+    "comment",
+    "# { total: 1, pass: 1 }\\n",
+  ],
+  Array [
+    "line",
+    "# duration_ms 9.118\\n",
+  ],
+  Array [
+    "comment",
+    "# duration_ms 9.118\\n",
+  ],
+  Array [
+    "complete",
+    FinalResults {
+      "bailout": false,
+      "count": 1,
+      "fail": 0,
+      "failures": Array [],
+      "ok": true,
+      "pass": 1,
+      "passes": undefined,
+      "plan": FinalPlan {
+        "comment": "",
+        "end": 1,
+        "skipAll": false,
+        "skipReason": "",
+        "start": 1,
+      },
+      "skip": 0,
+      "skips": Array [],
+      "time": 9.118,
+      "todo": 0,
+      "todos": Array [],
+    },
+  ],
+]
+`
+
+exports[`test/parser.ts > TAP > duration_ms.tap > output bail=true 1`] = `
+Array [
+  Array [
+    "line",
+    "TAP version 14\\n",
+  ],
+  Array [
+    "version",
+    14,
+  ],
+  Array [
+    "line",
+    "# Subtest: subtest\\n",
+  ],
+  Array [
+    "child",
+    Array [
+      Array [
+        "comment",
+        "# Subtest: subtest\\n",
+      ],
+      Array [
+        "line",
+        "ok 1 - this is fine\\n",
+      ],
+      Array [
+        "assert",
+        Result {
+          "buffered": false,
+          "diag": null,
+          "fullname": "subtest > this is fine",
+          "id": 1,
+          "name": "this is fine",
+          "ok": true,
+          "plan": null,
+          "previous": null,
+          "skip": false,
+          "tapError": null,
+          "time": null,
+          "todo": false,
+        },
+      ],
+      Array [
+        "line",
+        "1..1\\n",
+      ],
+      Array [
+        "plan",
+        Plan {
+          "comment": "",
+          "end": 1,
+          "start": 1,
+        },
+      ],
+      Array [
+        "complete",
+        FinalResults {
+          "bailout": false,
+          "count": 1,
+          "fail": 0,
+          "failures": Array [],
+          "ok": true,
+          "pass": 1,
+          "passes": undefined,
+          "plan": FinalPlan {
+            "comment": "",
+            "end": 1,
+            "skipAll": false,
+            "skipReason": "",
+            "start": 1,
+          },
+          "skip": 0,
+          "skips": Array [],
+          "time": null,
+          "todo": 0,
+          "todos": Array [],
+        },
+      ],
+      Array [
+        "close",
+      ],
+    ],
+  ],
+  Array [
+    "line",
+    "    ok 1 - this is fine\\n",
+  ],
+  Array [
+    "result",
+    Result {
+      "buffered": false,
+      "diag": null,
+      "fullname": "subtest > this is fine",
+      "id": 1,
+      "name": "this is fine",
+      "ok": true,
+      "plan": null,
+      "previous": null,
+      "skip": false,
+      "tapError": null,
+      "time": null,
+      "todo": false,
+    },
+  ],
+  Array [
+    "pass",
+    Result {
+      "buffered": false,
+      "diag": null,
+      "fullname": "subtest > this is fine",
+      "id": 1,
+      "name": "this is fine",
+      "ok": true,
+      "plan": null,
+      "previous": null,
+      "skip": false,
+      "tapError": null,
+      "time": null,
+      "todo": false,
+    },
+  ],
+  Array [
+    "line",
+    "    1..1\\n",
+  ],
+  Array [
+    "line",
+    "ok 1 - subtest\\n",
+  ],
+  Array [
+    "line",
+    "  ---\\n",
+  ],
+  Array [
+    "line",
+    "  duration_ms: 1.432\\n",
+  ],
+  Array [
+    "line",
+    "  ...\\n",
+  ],
+  Array [
+    "assert",
+    Result {
+      "buffered": false,
+      "diag": Object {},
+      "fullname": "subtest",
+      "id": 1,
+      "name": "subtest",
+      "ok": true,
+      "plan": null,
+      "previous": null,
+      "skip": false,
+      "tapError": null,
+      "time": 1.432,
+      "todo": false,
+    },
+  ],
+  Array [
+    "line",
+    "1..1\\n",
+  ],
+  Array [
+    "plan",
+    Plan {
+      "comment": "",
+      "end": 1,
+      "start": 1,
+    },
+  ],
+  Array [
+    "line",
+    "# { total: 1, pass: 1 }\\n",
+  ],
+  Array [
+    "comment",
+    "# { total: 1, pass: 1 }\\n",
+  ],
+  Array [
+    "line",
+    "# duration_ms 9.118\\n",
+  ],
+  Array [
+    "comment",
+    "# duration_ms 9.118\\n",
+  ],
+  Array [
+    "complete",
+    FinalResults {
+      "bailout": false,
+      "count": 1,
+      "fail": 0,
+      "failures": Array [],
+      "ok": true,
+      "pass": 1,
+      "passes": undefined,
+      "plan": FinalPlan {
+        "comment": "",
+        "end": 1,
+        "skipAll": false,
+        "skipReason": "",
+        "start": 1,
+      },
+      "skip": 0,
+      "skips": Array [],
+      "time": 9.118,
+      "todo": 0,
+      "todos": Array [],
+    },
+  ],
+]
+`
