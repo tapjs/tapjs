81c4ea7e3081608ccf1bbbe6dd576d6c0b082403
isaacs <i@izs.me>
2023-09-18 09:25:50 -0700

    reporter: remove hacky workaround for hanging indent
    No longer needed in ink 4.4

--- src/reporter/src/hanging-indent.tsx
+++ src/reporter/src/hanging-indent.tsx
@@ -1,24 +1,13 @@
 import { Text, TextProps, Transform } from 'ink'
 import React, { FC } from 'react'
 
-// kind of a weird way to add a hanging indent for wrapped lines,
-// but it seems to work.
-// See: https://github.com/vadimdemedes/ink/issues/611
-const sigil = '\x01\x02\x03\x03\x02\x01'
 export const HangingIndent: FC<{ indent?: number } & TextProps> = ({
   indent = 4,
   children,
   ...props
 }) => (
   <Transform
-    transform={o =>
-      o.includes(sigil)
-        ? o.replace(sigil, '')
-        : ' '.repeat(indent) + o
-    }>
-    <Text {...props}>
-      {sigil}
-      {children}
-    </Text>
+    transform={(o, i) => (i === 0 ? o : ' '.repeat(indent) + o)}>
+    <Text {...props}>{children}</Text>
   </Transform>
 )
