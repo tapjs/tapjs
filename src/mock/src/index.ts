5ce437124c9433c75efdba334ac30104d6854d6f
Nikita Pavlov <np@lsvr.ru>
2023-10-10 02:44:20 +0000

    fix: object with prototype mocking

--- src/mock/src/index.ts
+++ src/mock/src/index.ts
@@ -88,7 +88,7 @@ export class TapMock {
           return [k, v]
         })
         .concat(
-          Object.entries(overrides).filter(([k]) => !Object.hasOwnProperty(bases, k))
+          Object.entries(overrides).filter(([k]) => !Object.hasOwnProperty.call(bases, k))
         )
     ) as MockedObject<B, O>
 
